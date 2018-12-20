2792fc00a6671c19ec32715511807f8429177433
Anthony Raimondo <anthonyraimondo7@gmail.com>
2018-12-20 16:13:16 -0500

    Create clear_logs.py

---
+++ src/utilities/clear_logs.py
@@ -0,0 +1,8 @@
+import os
+
+
+parent_dir = "C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\logs"
+for sub_dir in os.listdir(parent_dir):
+    for log_file in os.listdir(parent_dir + '\\' + sub_dir):
+        f = open(parent_dir + '\\' + sub_dir + '\\' + log_file, 'w')
+        f.close()
