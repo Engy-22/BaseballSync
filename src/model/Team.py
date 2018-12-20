ed31e66892a36441d4ec4f2466cf9e4d0f928101
Anthony Raimondo <anthonyraimondo7@gmail.com>
2018-12-20 11:26:00 -0500

    Create Team.py

---
+++ src/model/Team.py
@@ -0,0 +1,82 @@
+from Model.DB_Connect import DB_Connect
+
+
+class Team:
+
+    def __init__(self, team_id: str, year: int):
+        self.teamId = team_id
+        self.year = year
+        self.roster = []
+        self.wins = 0
+        self.loses = 0
+        self.offRank_year = None
+        self.defRank_year = None
+        self.ovrRank_year = None
+        self.offRank_ovr = None
+        self.defRank_ovr = None
+        self.ovrRank_ovr = None
+        self.imageURL = ""
+
+
+    def addPlayerToRoster(self, playerId):
+        self.roster.append(playerId)
+
+### Getters ###
+    def getTeamId(self):
+        return self.teamId
+
+
+    def getYear(self):
+        return self.year
+
+
+    def getRoster(self):
+        return self.roster
+
+
+    def getWins(self):
+        return self.wins
+
+
+    def getLoses(self):
+        return self.loses
+
+
+    def getOffRank_year(self):
+        return self.offRank_year
+
+
+    def getDefRank_year(self):
+        return self.defRank_year
+
+
+    def getOvrRank_year(self):
+        return self.ovrRank_year
+
+
+    def getOffRank_ovr(self):
+        return self.offRank_ovr
+
+
+    def getDefRank_ovr(self):
+        return self.defRank_ovr
+
+
+    def getOvrRank_ovr(self):
+        return self.ovrRank_ovr
+
+
+    def getTeamImage(self):
+        return self.imageURL
+### Getters ###
+
+### SETTERS ###
+    def setRoster(self):
+        db, cursor = DB_Connect.grab("baseballData")
+        team_id = DB_Connect.read(cursor, "select TY_uniqueidentifier from team_years where teamId = '"+self.teamId+"' and year = "+str(self.year)+";")
+        print(team_id)
+        #data = DB_Connect.read(cursor, "select playerId from playerPositions where TY_uniqueidentifier = " + str(team_id))
+### SETTERS ###
+
+team = Team("CLE", 2017)
+team.setRoster()
