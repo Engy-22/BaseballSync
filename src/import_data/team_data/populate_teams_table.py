d4f6f8672cd0cccc0c96d3e9a18b2570984bc612
Anthony Raimondo <anthonyraimondo7@gmail.com>
2018-12-24 15:21:19 -0500

    Update populate_teams_table.py

--- src/import_data/team_data/populate_teams_table.py
+++ src/import_data/team_data/populate_teams_table.py
@@ -1,17 +1,17 @@
 from utilities.DB_Connect import DB_Connect
 from utilities.translate_team_name import translate_team_name
-import logging
+from utilities.Logger import Logger
 import datetime
 import time
 
-logging.basicConfig(filename="C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\logs\\import_data\\"
-                             "populate_teams_table.log", level=logging.DEBUG)
+logger = Logger("C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\logs\\import_data\\populate_teams_table."
+                "log")
 
 
 def populate_teams_table(year):
     print("populating teams table")
     start_time = time.time()
-    logging.info('\tBegin populating teams table for ' + str(year) + ' || Timestamp: ' + datetime.datetime.today()\
+    logger.log('\tBegin populating teams table for ' + str(year) + ' || Timestamp: ' + datetime.datetime.today()\
                  .strftime('%Y-%m-%d %H:%M:%S'))
     with open('C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\background\\yearTeams.txt', 'rt') as file:
         db, cursor = DB_Connect.grab("baseballData")
@@ -25,7 +25,7 @@ def populate_teams_table(year):
                 break
     DB_Connect.close(db)
     total_time = round(time.time() - start_time, 2)
-    logging.info('\tpopulating teams table completed: ' + str(total_time) + ' seconds\n\n')
+    logger.log('\tpopulating teams table completed: ' + str(total_time) + ' seconds\n\n')
 
 
 # populate_teams_table(2018)
