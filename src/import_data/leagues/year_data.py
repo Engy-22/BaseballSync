065efb725018fe2312a95d14a7038b88e6ee61b7
Anthony Raimondo <anthonyraimondo7@gmail.com>
2018-12-24 15:21:24 -0500

    Update year_data.py

--- src/import_data/leagues/year_data.py
+++ src/import_data/leagues/year_data.py
@@ -1,23 +1,21 @@
 from collections import Counter
-from concurrent.futures import ProcessPoolExecutor
 from concurrent.futures import ThreadPoolExecutor
 from urllib.request import urlopen
 import time
-import logging
 import datetime
 from bs4 import BeautifulSoup
 from utilities.DB_Connect import DB_Connect
+from utilities.Logger import Logger
 
 pages = {}
 strings = {}
-logging.basicConfig(filename="C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\logs\\import_data\\"
-                             "year_data.log", level=logging.DEBUG)
+logger = Logger("C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\logs\\import_data\\year_data.log")
 
 
 def get_year_data(year):
     print("Gathering year data")
     start_time = time.time()
-    logging.info('\tBeginning year_data download for ' + str(year) + ' || Timestamp: ' + datetime.datetime.today()\
+    logger.log('\tBeginning year_data download for ' + str(year) + ' || Timestamp: ' + datetime.datetime.today()\
                  .strftime('%Y-%m-%d %H:%M:%S'))
     batting_list = {'PA': 'pa', 'AB': 'ab', 'R': 'r', 'H': 'h', '2B': '2b', '3B': '3b', 'HR': 'hr', 'RBI': 'rbi',
                     'SB': 'sb', 'BB': 'bb', 'SO': 'so', 'batting_avg': 'ba', 'onbase_perc': 'obp',
@@ -32,25 +30,25 @@ def get_year_data(year):
     DB_Connect.close(db)
     write_opening_day(year)
     download_start = time.time()
-    logging.info("\tmaking HTTP requests for year data")
+    logger.log("\tmaking HTTP requests for year data")
     with ThreadPoolExecutor() as executor1:
         for key, value in stat_list.items():
             executor1.submit(load_url, year, key)
-    logging.info("\t\tdone making HTTP requests: time = " + str(round(time.time() - download_start, 2)))
+    logger.log("\t\tdone making HTTP requests: time = " + str(round(time.time() - download_start, 2)))
     for key, dictionary in stat_list.items():
         assemble_stats(key, dictionary, pages[key])
     write_start = time.time()
-    logging.info("\twriting to database")
+    logger.log("\twriting to database")
     with ThreadPoolExecutor() as executor2:
         for key, value in stat_list.items():
             executor2.submit(write_to_db, year, strings[key], key)
-    logging.info("\t\tdone writing to database: time = " + str(round(time.time() - write_start, 2)))
+    logger.log("\t\tdone writing to database: time = " + str(round(time.time() - write_start, 2)))
     total_time = round(time.time() - start_time, 2)
-    logging.info('\tyear_data download completed: time = ' + str(total_time) + ' seconds\n\n')
+    logger.log('\tyear_data download completed: time = ' + str(total_time) + ' seconds\n\n')
 
 
 def load_url(year, stat_type):
-    logging.info("\tdownloading " + stat_type + " data")
+    logger.log("\tdownloading " + stat_type + " data")
     pages[stat_type] = BeautifulSoup(urlopen("https://www.baseball-reference.com/leagues/MLB/" + str(year)
                                              + "-standard-" + stat_type + ".shtml"), 'html.parser')
 
@@ -58,7 +56,7 @@ def load_url(year, stat_type):
 def write_opening_day(year):
     start_time = time.time()
     db, cursor = DB_Connect.grab("baseballData")
-    logging.info('\tGetting date of opening day')
+    logger.log('\tGetting date of opening day')
     mlb_schedule = str(BeautifulSoup(urlopen('https://www.baseball-reference.com/leagues/MLB/' + str(year)
                                              + '-schedule.shtml'), 'html.parser'))
     opening_date = mlb_schedule.split('<h3>')[1].split('</h3>')[0]
@@ -68,13 +66,13 @@ def write_opening_day(year):
     DB_Connect.write(db, cursor, 'update years set opening_day = "' + opening_day + '" where year = ' + str(year) + ';')
     total_time = round(time.time() - start_time, 2)
     DB_Connect.close(db)
-    logging.info('\t\tComplete (opening day): time = ' + str(total_time) + ' seconds')
+    logger.log('\t\tComplete (opening day): time = ' + str(total_time) + ' seconds')
 
 
 def assemble_stats(stat_type, stats, page):
     page_string = str(page)
     start_time = time.time()
-    logging.info('\tAssembling year ' + stat_type + ' stats')
+    logger.log('\tAssembling year ' + stat_type + ' stats')
     lat_line2 = page_string.split('</tbody>')[1].split('</tfoot>')[0]
     this_string = ""
     for i in lat_line2.split('<td'):
@@ -92,12 +90,12 @@ def assemble_stats(stat_type, stats, page):
         this_string = 'g = ' + str(games) + ', ' + this_string
     strings[stat_type] = this_string
     total_time = round(time.time() - start_time, 2)
-    logging.info('\t\tCompleted (' + stat_type + '): time = ' + str(total_time) + ' seconds')
+    logger.log('\t\tCompleted (' + stat_type + '): time = ' + str(total_time) + ' seconds')
     strings[stat_type] = this_string
 
 
 def write_to_db(year, stat_string, stat_type):
-    logging.info("\twriting " + stat_type + " stats to database")
+    logger.log("\twriting " + stat_type + " stats to database")
     db, cursor = DB_Connect.grab("baseballData")
     DB_Connect.write(db, cursor, 'update years set ' + stat_string[:-2] + ' where year = ' + str(year) + ';')
     DB_Connect.close(db)
