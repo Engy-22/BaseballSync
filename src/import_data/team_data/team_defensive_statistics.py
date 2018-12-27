7b17f15a30e7d040c559b80d317cd78ab068b7b6
Anthony Raimondo <anthonyraimondo7@gmail.com>
2018-12-27 15:29:46 -0500

    Create team_defensive_statistics.py

---
+++ src/import_data/team_data/team_defensive_statistics.py
@@ -0,0 +1,8 @@
+from utilities.Logger import Logger
+
+logger = Logger("C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\logs\\import_data\\"
+                "team_defensive_statistics.log")
+
+
+def team_defensive_statistics(year, driver_logger):
+    pass
