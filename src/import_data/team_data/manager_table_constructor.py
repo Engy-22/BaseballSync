9658a0785e6523078e6fad6caf9bfc3398c1b14e
Anthony Raimondo <anthonyraimondo7@gmail.com>
2018-12-26 21:51:10 -0500

    Create manager_table_constructor.py

---
+++ src/import_data/team_data/manager_table_constructor.py
@@ -0,0 +1,45 @@
+import os
+import urllib.request
+from bs4 import BeautifulSoup as bs
+from concurrent.futures import ThreadPoolExecutor
+from utilities.DB_Connect import DB_Connect
+from utilities.Logger import Logger
+import datetime
+import time
+
+logger = Logger('C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\logs\\import_data\\'
+                'manager_table_constructor.log')
+
+
+def manager_table_constructor():
+    print("Gathering manager data (all-time)")
+    start_time = time.time()
+    logger.log('Begin populating teams table || Timestamp: ' + datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
+    table = str(bs(urllib.request.urlopen('https://www.baseball-reference.com/managers/'), 'html.parser'))
+    rows = table.split('<tr')
+    with ThreadPoolExecutor(os.cpu_count()) as executor:
+        for row in rows:
+            if '<td class="left " csk="' in row:
+                this_row = row.split('</tr>')[0]
+                try:
+                    manager_id = this_row.split('<a href="/managers/')[1].split('.shtml')[0].replace("'", "\'")
+                    last_first = this_row.split('</tr>')[0].split('<td class="left " csk="')[1].split('"')[0]
+                    last = last_first.split(',')[0].replace("'", "\'")
+                    first = last_first.split(',')[1].replace("'", "\'")
+                    wins = this_row.split('data-stat="W">')[1].split('<')[0]
+                    loses = this_row.split('data-stat="L">')[1].split('<')[0]
+                    executor.submit(write_to_file, '"' + manager_id + '","' + last + '","' + first + '",' + wins + ','
+                                                   + loses)
+                except AttributeError:
+                    continue
+    logger.log('Constructing manager table completed: time = ' + (str(round(time.time() - start_time, 2))) + '\n\n')
+
+
+def write_to_file(data):
+    db, cursor = DB_Connect.grab("baseballData")
+    DB_Connect.write(db, cursor, 'insert into managers (managerId, lastName, firstName, wins, loses) values (' + data
+                     + ');')
+    DB_Connect.close(db)
+
+
+# manager_table_constructor()
