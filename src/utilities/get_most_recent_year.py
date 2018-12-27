fb5119196dce04793f14a013f4b3c6539417caf9
Anthony Raimondo <anthonyraimondo7@gmail.com>
2018-12-26 23:49:43 -0500

    Update get_most_recent_year.py

--- src/utilities/get_most_recent_year.py
+++ src/utilities/get_most_recent_year.py
@@ -1,7 +1,11 @@
 from utilities.DB_Connect import DB_Connect
+from utilities.Logger import Logger
+
+driver_logger = Logger("C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\logs\\import_data\\driver.log")
 
 
 def get_most_recent_year():
+    driver_logger.log('Getting most recent year listed in the database')
     db, cursor = DB_Connect.grab("baseballData")
     most_recent_year = int(DB_Connect.read(cursor, "select year from years order by year desc limit 1;")[0][0])
     DB_Connect.close(db)
