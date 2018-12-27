267c58a1189eb0bf6d6d72802af9835ba252f4a4
Anthony Raimondo <anthonyraimondo7@gmail.com>
2018-12-26 23:49:56 -0500

    Update league_table_constructor.py

--- src/import_data/leagues/league_table_constructor.py
+++ src/import_data/leagues/league_table_constructor.py
@@ -2,11 +2,13 @@ from utilities.DB_Connect import DB_Connect
 from utilities.Logger import Logger
 import datetime
 
+driver_logger = Logger("C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\logs\\import_data\\driver.log")
 logger = Logger("C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\logs\\import_data\\"
                 "league_table_constructor.log")
 
 
 def league_table_constructor():
+    driver_logger.log('Populating leagues table (all-time)')
     print('Populating leagues table (all-time)')
     logger.log('Begin populating leagues table || Timestamp: ' + datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
     leagues = {'NL': 'National League',
@@ -22,6 +24,7 @@ def league_table_constructor():
                                      + league_name + '");')
     DB_Connect.close(db)
     logger.log('Populating leagues table completed\n\n')
+    driver_logger.log('\tPopulating leagues table completed')
 
 
 # league_table_constructor()
