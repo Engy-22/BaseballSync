e7e271b6ca9590c9d4bf5a4c38c8f2da9cc81217
Anthony Raimondo <anthonyraimondo7@gmail.com>
2018-12-26 21:50:54 -0500

    Create league_table_constructor.py

---
+++ src/import_data/leagues/league_table_constructor.py
@@ -0,0 +1,27 @@
+from utilities.DB_Connect import DB_Connect
+from utilities.Logger import Logger
+import datetime
+
+logger = Logger("C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\logs\\import_data\\"
+                "league_table_constructor.log")
+
+
+def league_table_constructor():
+    print('Populating leagues table (all-time)')
+    logger.log('Begin populating leagues table || Timestamp: ' + datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
+    leagues = {'NL': 'National League',
+               'AL': 'American League',
+               'AA': 'American Association',
+               'FL': 'Federal League',
+               'PL': 'Players League',
+               'UA': 'Union Association',
+               'NA': 'National Association'}
+    db, cursor = DB_Connect.grab("baseballData")
+    for league_id, league_name in leagues.items():
+        DB_Connect.write(db, cursor, 'insert into leagues (leagueId, leagueName) values ("' + league_id + '", "'
+                                     + league_name + '");')
+    DB_Connect.close(db)
+    logger.log('Populating leagues table completed\n\n')
+
+
+# league_table_constructor()
