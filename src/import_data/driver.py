b65dfa95d7573345e17e724c0859ba9874103f17
Anthony Raimondo <anthonyraimondo7@gmail.com>
2018-12-26 23:49:54 -0500

    Update driver.py

--- src/import_data/driver.py
+++ src/import_data/driver.py
@@ -3,12 +3,18 @@
     This populates all tables in the baseballData database
 """
 
+import time
+import datetime
+from utilities.Logger import Logger
+from utilities.time_converter import time_converter
 from utilities.get_most_recent_year import get_most_recent_year
 from import_data.leagues.league_table_constructor import league_table_constructor
 from import_data.team_data.manager_table_constructor import manager_table_constructor
 from import_data.team_data.populate_teams_table import populate_teams_table
 from import_data.leagues.year_data import get_year_data
 from import_data.team_data.ballpark_and_manager_data import ballpark_and_manager_data
+
+logger = Logger("C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\logs\\import_data\\driver.log")
 
 
 def driver(year):
@@ -19,6 +25,8 @@ def driver(year):
 
 
 if __name__ == '__main__':
+    logger.log('Begin Driver || Timestamp: ' + datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S'))
+    start_time = time.time()
     try:
         most_recent_year = get_most_recent_year()
     except IndexError:
@@ -26,6 +34,7 @@ if __name__ == '__main__':
     league_table_constructor()
     manager_table_constructor()
     driver(2018)
-# if most_recent_year > 1997:
-#     comparisons_driver()
-# hof_finder()
+    # if most_recent_year > 1997:
+    #     comparisons_driver()
+    # hof_finder()
+    logger.log('\nDriver complete: time = ' + time_converter(time.time() - start_time))
