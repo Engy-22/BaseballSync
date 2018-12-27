372eb0563d51e9a2c168628e0981651de8aeacad
Anthony Raimondo <anthonyraimondo7@gmail.com>
2018-12-27 17:59:51 -0500

    Update driver.py

--- src/import_data/driver.py
+++ src/import_data/driver.py
@@ -16,6 +16,7 @@ from import_data.team_data.ballpark_and_manager_data import ballpark_and_manager
 from import_data.leagues.league_standings import league_standings
 from import_data.team_data.team_offensive_statistics import team_offensive_statistics
 from import_data.team_data.team_defensive_statistics import team_defensive_statistics
+from import_data.player_data.pitch_fx import get_pitch_fx_data
 
 
 def driver(year, driver_log):
@@ -26,6 +27,7 @@ def driver(year, driver_log):
     league_standings(year, driver_log)
     team_offensive_statistics(year, driver_log)
     team_defensive_statistics(year, driver_log)
+    # get_pitch_fx_data(year, driver_log)
 
 
 if __name__ == '__main__':
