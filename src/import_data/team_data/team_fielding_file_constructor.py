81ae522ea9be0318e4e6b75e29a85fdd9aa85cc7
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-08 17:04:04 -0500

    Update team_fielding_file_constructor.py

--- src/import_data/team_data/team_fielding_file_constructor.py
+++ src/import_data/team_data/team_fielding_file_constructor.py
@@ -8,6 +8,7 @@ from utilities.time_converter import time_converter
 from utilities.Logger import Logger
 from concurrent.futures import ThreadPoolExecutor
 
+pages = {}
 logger = Logger("C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\logs\\import_data\\"
                 "team_fielding_file_constructor.log")
 
@@ -18,6 +19,7 @@ def team_fielding_file_constructor(year, driver_logger):
     start_time = time.time()
     logger.log("Downloading " + str(year) + " team fielding positions || Timestamp: " + datetime.datetime.today()\
                .strftime('%Y-%m-%d %H:%M:%S'))
+    logger.log("\tDownloading team pages")
     with open("C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\background\\yearTeams.txt", 'r') as year_file:
         with ThreadPoolExecutor(os.cpu_count()) as executor:
             for line in year_file:
@@ -26,48 +28,58 @@ def team_fielding_file_constructor(year, driver_logger):
                     for team in temp_line:
                         split_team = team.split(';')
                         if "TOT" not in split_team:
-                            executor.submit(write_to_file, split_team[0], split_team[1], year)
+                            executor.submit(load_url, year, split_team[0], split_team[1])
                     break
+    logger.log("\t\tTime = " + time_converter(time.time() - start_time))
+    logger.log("\tOrganizing team position data")
+    write_time = time.time()
+    write_to_file(year)
+    logger.log("\t\tTime = " + time_converter(time.time() - write_time))
     total_time = time_converter(time.time() - start_time)
     logger.log("Done downloading team fielding data: time = " + total_time + '\n\n')
     driver_logger.log("\t\tTime = " + total_time)
 
 
-def write_to_file(team, team_key, year):
-    logger.log("\tgathering and writing " + team + " positions")
-    db, cursor = DB_Connect.grab("baseballData")
-    primary_keys = []
-    try:
-        table = str(BeautifulSoup(urlopen("https://www.baseball-reference.com/teams/" + team_key + "/" + str(year) + "-"
-                                          "fielding.shtml"), "html.parser")).split('<tbody>')[1].split('</tbody>')[0]\
-            .split('<tr')
-        for row in table:
-            if 'data-append-csv="' in row:
-                this_string = ""
-                primary_keys.append(row.split('data-append-csv="')[1].split('"')[0].replace("'", "\'"))
-                this_string += '"' + primary_keys[-1] + '","'
-                position_summary = row.split('data-stat="pos_summary" >')[1].split('<')[0]
-                if '-' in position_summary:
-                    positions = position_summary.split('-')
-                    for i in range(len(positions)):
-                        if i != len(positions)-1:
-                            this_string += positions[i] + ","
-                        else:
-                            this_string += positions[i]
-                else:
-                    this_string += position_summary
-                this_string += '"'
-                if len(DB_Connect.read(cursor, 'select PPos_uniqueidentifier from player_positions where playerId = '
-                                               + this_string.split(',')[0] + ' and TY_uniqueidentifier = (select '
-                                               + 'TY_uniqueidentifier from team_years where teamId = "' + team
-                                               + '" and year = ' + str(year) + ');')) == 0:
-                    DB_Connect.write(db, cursor, 'insert into player_positions (PPos_uniqueidentifier, playerId, '
-                                                 'positions, TY_uniqueidentifier) values (default, ' + this_string
-                                                 + ', (select TY_uniqueidentifier from team_years where teamId = "'
-                                                 + team + '" and year = ' + str(year) + '));')
-    except IndexError:
-        pass
-    DB_Connect.close(db)
+def load_url(year, team_id, team_key):
+    logger.log("\t\tdownloading " + team_id + " page")
+    pages[team_id] = str(BeautifulSoup(urlopen("https://www.baseball-reference.com/teams/" + team_key + "/" + str(year)
+                                               + "-fielding.shtml"), "html.parser")).split('<tbody>')[1]\
+        .split('</tbody>')[0].split('<tr')
+
+
+def write_to_file(year):
+    for team_id, table in pages.items():
+        logger.log("\t\tgathering and writing " + team_id + " positions")
+        db, cursor = DB_Connect.grab("baseballData")
+        primary_keys = []
+        try:
+            for row in table:
+                if 'data-append-csv="' in row:
+                    this_string = ""
+                    primary_keys.append(row.split('data-append-csv="')[1].split('"')[0].replace("'", "\'"))
+                    this_string += '"' + primary_keys[-1] + '","'
+                    position_summary = row.split('data-stat="pos_summary" >')[1].split('<')[0]
+                    if '-' in position_summary:
+                        positions = position_summary.split('-')
+                        for i in range(len(positions)):
+                            if i != len(positions)-1:
+                                this_string += positions[i] + ","
+                            else:
+                                this_string += positions[i]
+                    else:
+                        this_string += position_summary
+                    this_string += '"'
+                    if len(DB_Connect.read(cursor, 'select PPos_uniqueidentifier from player_positions where playerId='
+                                                   + this_string.split(',')[0] + ' and TY_uniqueidentifier = (select '
+                                                   + 'TY_uniqueidentifier from team_years where teamId = "' + team_id
+                                                   + '" and year = ' + str(year) + ');')) == 0:
+                        DB_Connect.write(db, cursor, 'insert into player_positions (PPos_uniqueidentifier, playerId, '
+                                                     'positions, TY_uniqueidentifier) values (default, ' + this_string
+                                                     + ', (select TY_uniqueidentifier from team_years where teamId = "'
+                                                     + team_id + '" and year = ' + str(year) + '));')
+        except IndexError:
+            pass
+        DB_Connect.close(db)
 
 
 # team_fielding_file_constructor(2018, Logger("C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\logs\\"
