ad155f668454966c5c552bcccd9173cdf51af47b
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-09 18:31:42 -0500

    Create stringify_list.py

---
+++ src/utilities/stringify_list.py
@@ -0,0 +1,10 @@
+def stringify_list(given_list):
+    if len(given_list) > 1:
+        low = str(given_list[0])
+        high = str(given_list[-1])
+        if len(given_list) != 2:
+            return 's ' + low + '-' + high
+        else:
+            return 's ' + low + ' & ' + high
+    else:
+        return str(given_list[0])
