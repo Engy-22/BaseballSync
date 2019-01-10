dfb1353fd91e7384549e1438c6d37eee4e0d845d
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-10 09:39:03 -0500

    Update comparisons_driver.py

--- src/import_data/comparisions/comparisons_driver.py
+++ src/import_data/comparisions/comparisons_driver.py
@@ -1,6 +1,8 @@
+import time
+import datetime
+from utilities.Logger import Logger
 from import_data.comparisions.stat_gatherers.gather_players import gather_players
 from import_data.comparisions.stat_gatherers.gather_teams import gather_teams
-from utilities.get_most_recent_year import get_most_recent_year
 from import_data.comparisions.stat_gatherers.batting_comparison_engine import make_hitter_comparisons, hitter_dr_calc
 from import_data.comparisions.getters.hitting_getters import get_year_totals as hitter_year_totals, get_hitter_stats
 from import_data.comparisions.stat_gatherers.offensive_comparison_engine import make_offensive_comparisons, offensive_dr_calc
@@ -11,7 +13,7 @@ from import_data.comparisions.stat_gatherers.defensive_comparison_engine import 
 from import_data.comparisions.getters.defensive_team_getters import get_defensive_stats, get_year_totals as defensive_year_totals
 
 
-def comparisons_driver():
+def comparisons_driver(most_recent_year, driver_logger):
     possible_hitter_comps = {}
     hitters_to_compare = {}
     possible_pitcher_comps = {}
@@ -22,7 +24,7 @@ def comparisons_driver():
     defenses_to_compare = {}
 
     print('making hitter comparisons (overall)')
-    hitter_years_to_compare = [year for year in range(get_most_recent_year(), 1997, -1)]
+    hitter_years_to_compare = [year for year in range(most_recent_year, 1997, -1)]
     for year_to_compare in hitter_years_to_compare:
         possible_hitter_comps[year_to_compare] = {}
         year_pa, year_totals = hitter_year_totals(year_to_compare)
@@ -32,11 +34,11 @@ def comparisons_driver():
             if comp_team_pa >= 300:
                 possible_hitter_comps[year_to_compare][comp_ty_uid + ';' + str(year_to_compare)] =\
                     hitter_dr_calc(comp_team_pa, comp_stats, year_pa, year_totals)
-    for comp_year in range(1876, get_most_recent_year() + 1, 1):
+    for comp_year in range(1876, most_recent_year + 1, 1):
         make_hitter_comparisons(gather_players(comp_year, "batting"), comp_year, possible_hitter_comps)
 
     print('making pitcher comparisons (overall)')
-    pitcher_years_to_compare = [year for year in range(get_most_recent_year(), 1996, -1)]
+    pitcher_years_to_compare = [year for year in range(most_recent_year, 1996, -1)]
     for year_to_compare in pitcher_years_to_compare:
         possible_pitcher_comps[year_to_compare] = {}
         year_pa, year_totals = pitcher_year_totals(year_to_compare)
@@ -46,32 +48,32 @@ def comparisons_driver():
             if comp_pitcher_pa >= 300:
                 possible_pitcher_comps[year_to_compare][comp_pitcher + ';' + str(year_to_compare)] = \
                     pitcher_dr_calc(comp_pitcher_pa, comp_stats, year_pa, year_totals)
-    for comp_year in range(1876, get_most_recent_year() + 1, 1):
+    for comp_year in range(1876, most_recent_year + 1, 1):
         make_pitcher_comparisons(gather_players(comp_year, "pitching"), comp_year, possible_pitcher_comps)
 
     print('making team offensive comparisons')
-    offensive_years_to_compare = [year for year in range(get_most_recent_year(), 1997, -1)]
+    offensive_years_to_compare = [year for year in range(most_recent_year, 1997, -1)]
     for year_to_compare in offensive_years_to_compare:
         year_pa, year_totals = hitter_year_totals(year_to_compare)
         offenses_to_compare[year_to_compare] = gather_teams(year_to_compare)
         for comp_ty_uid in offenses_to_compare[year_to_compare]:
             comp_team_pa, comp_stats = get_offensive_stats(comp_ty_uid)
             possible_offensive_comps[comp_ty_uid] = offensive_dr_calc(comp_team_pa, comp_stats, year_pa, year_totals)
-    for comp_year in range(1876, get_most_recent_year() + 1, 1):
+    for comp_year in range(1876, most_recent_year + 1, 1):
         make_offensive_comparisons(gather_teams(comp_year), comp_year, possible_offensive_comps)
 
     print('making team defensive comparisons')
-    defensive_years_to_compare = [year for year in range(get_most_recent_year(), 1997, -1)]
+    defensive_years_to_compare = [year for year in range(most_recent_year, 1997, -1)]
     for year_to_compare in defensive_years_to_compare:
         year_pa, year_totals = defensive_year_totals(year_to_compare)
         defenses_to_compare[year_to_compare] = gather_teams(year_to_compare)
         for comp_ty_uid in defenses_to_compare[year_to_compare]:
             comp_team_pa, comp_stats = get_defensive_stats(comp_ty_uid)
             possible_defensive_comps[comp_ty_uid] = defensive_dr_calc(comp_team_pa, comp_stats, year_pa, year_totals)
-    for comp_year in range(1876, get_most_recent_year() + 1, 1):
+    for comp_year in range(1876, most_recent_year + 1, 1):
         make_defensive_comparisons(gather_teams(comp_year), comp_year, possible_defensive_comps)
 
 
-# start_time = time.time()
-# comparisons_driver()
-# print((time.time() - start_time) / 3600)
+# from utilities.get_most_recent_year import get_most_recent_year
+# dump = Logger("C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\logs\\import_data\\dump.log")
+# comparisons_driver(get_most_recent_year(dump), dump)
