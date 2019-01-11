fc0f5164b57bce3d805e6412792d877edf2684d3
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-11 10:19:26 -0500

    Update offensive_team_getters.py

--- src/import_data/comparisions/getters/offensive_team_getters.py
+++ src/import_data/comparisions/getters/offensive_team_getters.py
@@ -1,7 +1,7 @@
 from utilities.DB_Connect import DB_Connect
 
 
-def get_offensive_stats(ty_uid):
+def get_offensive_stats(ty_uid, driver_logger):
     stats = {'R': 0, 'H': 0, '2B': 0, '3B': 0, 'HR': 0, 'SB': 0, 'BB': 0, 'SO': 0}
     db, cursor = DB_Connect.grab("baseballData")
     pa = int(DB_Connect.read(cursor, 'select pa from team_years where ty_uniqueidentifier=' + str(ty_uid) + ';')[0][0])
@@ -12,7 +12,7 @@ def get_offensive_stats(ty_uid):
     return pa, stats
 
 
-def get_year_totals(year):
+def get_year_totals(year, driver_logger):
     year_totals = {'R': 0, 'H': 0, '2B': 0, '3B': 0, 'HR': 0, 'SB': 0, 'BB': 0, 'SO': 0}
     db, cursor = DB_Connect.grab("baseballData")
     pa = int(DB_Connect.read(cursor, 'select pa from years where year = ' + str(year) + ';')[0][0])
