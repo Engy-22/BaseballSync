40c5574a68a0b7700470156712e2410664f13aff
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-11 10:19:29 -0500

    Update pitching_getters.py

--- src/import_data/comparisions/getters/pitching_getters.py
+++ src/import_data/comparisions/getters/pitching_getters.py
@@ -1,7 +1,7 @@
 from utilities.DB_Connect import DB_Connect
 
 
-def get_pitcher_stats(pitcher, year):
+def get_pitcher_stats(pitcher, year, driver_logger):
     stats = {'IP': 0, 'H': 0, '2B': 0, '3B': 0, 'HR': 0, 'SO': 0, 'BB': 0, 'ER': 0}
     db, cursor = DB_Connect.grab("baseballData")
     pa = int(DB_Connect.read(cursor, 'select player_pitching.pa from player_pitching, player_teams where player_pitchi'
@@ -16,7 +16,7 @@ def get_pitcher_stats(pitcher, year):
     return pa, stats
 
 
-def get_year_totals(year):
+def get_year_totals(year, driver_logger):
     year_totals = {'IP': 0, 'H': 0, '2B': 0, '3B': 0, 'HR': 0, 'SO': 0, 'BB': 0, 'ER': 0}
     db, cursor = DB_Connect.grab("baseballData")
     pa = int(DB_Connect.read(cursor, 'select pa from years where year = ' + str(year) + ';')[0][0])
