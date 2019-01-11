00dc73dc1ef909e0ae1acdabd7c6e2da62fd906e
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-11 10:19:35 -0500

    Update gather_players.py

--- src/import_data/comparisions/stat_gatherers/gather_players.py
+++ src/import_data/comparisions/stat_gatherers/gather_players.py
@@ -1,7 +1,7 @@
 from utilities.DB_Connect import DB_Connect
 
 
-def gather_players(year, player_type):
+def gather_players(year, player_type, driver_logger):
     db, cursor = DB_Connect.grab("baseballData")
     player_list = list(DB_Connect.read(cursor, "select playerId from player_teams, player_" + player_type + " where"
                                                + " player_" + player_type + ".PT_uniqueidentifier = "
