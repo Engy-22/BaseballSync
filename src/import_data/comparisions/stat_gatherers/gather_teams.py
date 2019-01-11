cac70106d8cb58ecac3e61e63353a91579009966
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-11 10:19:38 -0500

    Update gather_teams.py

--- src/import_data/comparisions/stat_gatherers/gather_teams.py
+++ src/import_data/comparisions/stat_gatherers/gather_teams.py
@@ -1,7 +1,7 @@
 from utilities.DB_Connect import DB_Connect
 
 
-def gather_teams(year):
+def gather_teams(year, driver_logger):
     db, cursor = DB_Connect.grab("baseballData")
     ty_uids = list(DB_Connect.read(cursor, 'select ty_uniqueidentifier from team_years where year=' + str(year) + ';'))
     DB_Connect.close(db)
