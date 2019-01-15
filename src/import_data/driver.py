3e96bb9ae2f5e115c718ee69329801de43966883
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-15 13:15:50 -0500

    Update driver.py

--- src/import_data/driver.py
+++ src/import_data/driver.py
@@ -75,7 +75,7 @@ if __name__ == '__main__':
     league_table_constructor(main_logger)
     manager_table_constructor(main_logger)
     years = []
-    for year in range(1997, 1999, 1):
+    for year in range(1999, 2005, 1):
         years.append(year)
         driver(year, main_logger)
     # if most_recent_year > 1997:
