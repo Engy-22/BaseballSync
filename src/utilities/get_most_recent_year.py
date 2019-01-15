edeccd9b008e64bdf79df67189149e8b28938359
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-15 16:18:46 -0500

    Update get_most_recent_year.py

--- src/utilities/get_most_recent_year.py
+++ src/utilities/get_most_recent_year.py
@@ -4,8 +4,12 @@ from utilities.DB_Connect import DB_Connect
 def get_most_recent_year(driver_logger):
     driver_logger.log('\t* Getting most recent year listed in the database *')
     db, cursor = DB_Connect.grab("baseballData")
-    most_recent_year = int(DB_Connect.read(cursor, "select year from years order by year desc limit 1;")[0][0])
-    DB_Connect.close(db)
+    try:
+        most_recent_year = int(DB_Connect.read(cursor, "select year from years order by year desc limit 1;")[0][0])
+    except:
+        most_recent_year = 1876
+    finally:
+        DB_Connect.close(db)
     return most_recent_year
 
 
