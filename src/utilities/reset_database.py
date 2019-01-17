540d9029514ebb07cdbff7fa87f6948fa492f5b9
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-17 09:19:19 -0500

    Update reset_database.py

--- src/utilities/reset_database.py
+++ src/utilities/reset_database.py
@@ -1,13 +1,17 @@
-from utilities.DB_Connect import DB_Connect
+import os
+from utilities.dbconnect import DatabaseConnection
+from concurrent.futures import ThreadPoolExecutor
 
 
-db, cursor = DB_Connect.grab("baseballData")
+db = DatabaseConnection()
 with open("..\\..\\background\\table_definitions.txt", 'rt') as file:
     table_defs = file.readlines()
     print("removing existing tables")
-    for line in reversed(table_defs):
-        DB_Connect.write(db, cursor, "drop table " + line.split('create table ')[1].split(' (')[0])
+    with ThreadPoolExecutor(os.cpu_count()) as executor:
+        for line in reversed(table_defs):
+            executor.submit(db.write("drop table " + line.split('create table ')[1].split(' (')[0]))
     print("creating new tables")
-    for line in table_defs:
-        DB_Connect.write(db, cursor, line)
-DB_Connect.close(db)
+    with ThreadPoolExecutor(os.cpu_count()) as executor2:
+        for line in table_defs:
+            executor2.submit(db.write(line))
+db.close()
