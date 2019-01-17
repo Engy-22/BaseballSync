21dc10a7909b7be98871a436e617a3e5b7f679e5
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-17 09:19:06 -0500

    Update team_defensive_statistics.py

--- src/import_data/team_data/team_defensive_statistics.py
+++ src/import_data/team_data/team_defensive_statistics.py
@@ -18,13 +18,16 @@ def team_defensive_statistics(year, driver_logger):
     logger.log('Downloading team defensive data for ' + str(year) + ' || Timestamp: ' + datetime.datetime.today().\
                strftime('%Y-%m-%d %H:%M:%S'))
     page1 = str(BeautifulSoup(urlopen("https://www.baseball-reference.com/leagues/MLB/" + str(year)
-                                     + "-standard-pitching.shtml"), "html.parser"))
-    page2 = str(BeautifulSoup(urlopen("https://www.baseball-reference.com/leagues/MLB/" + str(year)
-                                      + "-batting-pitching.shtml"), "html.parser"))
+                                      + "-standard-pitching.shtml"), "html.parser"))
+    try:
+        page2 = str(BeautifulSoup(urlopen("https://www.baseball-reference.com/leagues/MLB/" + str(year)
+                                          + "-batting-pitching.shtml"), "html.parser"))
+        batting_against_rows = page2.split('Player Batting Against')[0].split('<h2>Team Batting Against')[1].\
+                                     split('<tbody>')[1].split('</tbody>')[0].split('<tr>')
+    except Exception:
+        batting_against_rows = []
     standard_pitching_rows = page1.split('Player Standard Pitching')[0].split('<h2>Team Standard Pitching')[1].\
                                    split('<tbody>')[1].split('</tbody>')[0].split('<tr>')
-    batting_against_rows = page2.split('Player Batting Against')[0].split('<h2>Team Batting Against')[1].\
-                                 split('<tbody>')[1].split('</tbody>')[0].split('<tr>')
     stats1 = {'R': 'RA', 'ER': 'ER', 'H': "HA", 'HR': 'HRA', 'BB': 'BBA', 'HBP': 'HBPA', 'IBB': 'IBBA', 'SO': 'K',
               'ERA': 'ERA', 'whip': 'WHIP'}
     stats2 = {'PA': 'PAA', 'AB': 'ABA', '2B': '2BA', '3B': '3BA', 'batting_avg': 'BAA', 'onbase_perc': 'OBA',
@@ -58,7 +61,10 @@ def write_to_file(team_data, year):
         logger.log("\tWriting " + team + " data to database")
         sets = ''
         for field, value in data.items():
-            sets += field + ' = ' + value + ', '
+            if value != '':
+                sets += field + ' = ' + value + ', '
+            else:
+                continue
         DB_Connect.write(db, cursor, 'update team_years set ' + sets[:-2] + ' where teamid = "' + team + '" and year = '
                                      + str(year) + ';')
     DB_Connect.close(db)
