296ab72391f6ca38ba7ca1868bd1831d60158f4a
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-18 09:55:52 -0500

    Update gather_players.py

--- src/import_data/comparisions/stat_gatherers/gather_players.py
+++ src/import_data/comparisions/stat_gatherers/gather_players.py
@@ -3,9 +3,9 @@ from utilities.DB_Connect import DB_Connect
 
 def gather_players(year, player_type, driver_logger):
     db, cursor = DB_Connect.grab("baseballData")
-    player_list = list(DB_Connect.read(cursor, "select playerId from player_teams, player_" + player_type + " where"
-                                               + " player_" + player_type + ".PT_uniqueidentifier = "
-                                               + " player_teams.PT_uniqueidentifier and player_" + player_type
-                                               + ".year = " + str(year) + ";"))
+    player_list = list(DB_Connect.read(cursor, "select playerId from player_teams, player_" + player_type + " where "
+                                               "player_" + player_type + ".PT_uniqueidentifier = player_teams."
+                                               "PT_uniqueidentifier and player_" + player_type + ".year = " + str(year)
+                                               + " and player_" + player_type + ".certainty <  1.0;"))
     DB_Connect.close(db)
     return [player[0] for player in player_list]
