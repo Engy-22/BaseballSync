d7b73138b366fef946d687f770b58de50067531f
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-18 08:07:48 -0500

    Update team_batting_order_constructor.py

--- src/import_data/team_data/team_batting_order_constructor.py
+++ src/import_data/team_data/team_batting_order_constructor.py
@@ -113,5 +113,5 @@ def stringify_num(num):
     return dictionary[num]
 
 
-# team_batting_order_constructor(2018, Logger("C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\logs\\"
-#                                             "import_data\\dump.log"))
+# dump_logger = Logger("C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\logs\\import_data\\dump.log")
+# team_batting_order_constructor(2018, dump_logger)
