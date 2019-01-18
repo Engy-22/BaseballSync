31a9006989261e52461d919a5ef0b6bd86d06049
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-18 13:26:20 -0500

    Update write_to_file_teams.py

--- src/import_data/comparisions/file_writers/write_to_file_teams.py
+++ src/import_data/comparisions/file_writers/write_to_file_teams.py
@@ -1,24 +1,22 @@
-from utilities.DB_Connect import DB_Connect
+from utilities.dbconnect import DatabaseConnection
 
 
 def write_to_file(comparisons, stat_type):
-    db, cursor = DB_Connect.grab("baseballData")
+    db = DatabaseConnection()
     for ty_uid, comp in comparisons.items():
         if comp is not None:
-            comp_pull = 1 - float(DB_Connect.read(cursor, 'select certainty from team_years where ty_uniqueidentifier '
-                                                          + '= ' + str(ty_uid) + ';')[0][0])
-            if len(DB_Connect.read(cursor, 'select comp_id from comparisons_team_' + stat_type + '_overall where '
-                                           + 'ty_uniqueidentifier = ' + str(ty_uid) + ';')) == 0:
-                DB_Connect.write(db, cursor, 'insert into comparisons_team_' + stat_type + '_overall (comp_id, '
-                                             'ty_uniqueidentifier, comp, comp_pull) values (default, ' + str(ty_uid)
-                                             + ', ' + str(comp) + ', ' + str(comp_pull) + ');')
+            comp_pull = 1 - float(db.read('select certainty from team_years where ty_uniqueidentifier = ' + str(ty_uid)
+                                          + ';')[0][0])
+            if len(db.read('select comp_id from comparisons_team_' + stat_type + '_overall where ty_uniqueidentifier = '
+                           + str(ty_uid) + ';')) == 0:
+                db.write('insert into comparisons_team_' + stat_type + '_overall (comp_id, ty_uniqueidentifier, comp, '
+                         'comp_pull) values (default, ' + str(ty_uid) + ', ' + str(comp) + ', ' + str(comp_pull) + ');')
             else:
-                DB_Connect.write(db, cursor, 'update comparisons_team_' + stat_type + '_overall set comp = ' + str(comp)
-                                             + ', comp_pull = ' + str(comp_pull) + ' where ty_uniqueidentifier = '
-                                             + str(ty_uid) + ';')
+                db.write('update comparisons_team_' + stat_type + '_overall set comp = ' + str(comp) + ', comp_pull = '
+                         + str(comp_pull) + ' where ty_uniqueidentifier = ' + str(ty_uid) + ';')
         else:
-            if len(DB_Connect.read(cursor, 'select comp_id from comparisons_team_' + stat_type + '_overall where '
-                                           + 'ty_uniqueidentifier = ' + str(ty_uid) + ';')) == 0:
-                DB_Connect.write(db, cursor, 'insert into comparisons_team_' + stat_type + '_overall (comp_id, '
-                                             + 'ty_uniqueidentifier) values (default, ' + str(ty_uid) + ');')
-    DB_Connect.close(db)
+            if len(db.read('select comp_id from comparisons_team_' + stat_type + '_overall where ty_uniqueidentifier = '
+                           + str(ty_uid) + ';')) == 0:
+                db.write('insert into comparisons_team_' + stat_type + '_overall (comp_id, ty_uniqueidentifier) values '
+                         '(default, ' + str(ty_uid) + ');')
+    db.close()
