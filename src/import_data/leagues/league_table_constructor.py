6d371c52c21ec3f5131ebebb2916481c7ba1b3a1
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-19 16:11:03 -0500

    Update league_table_constructor.py

--- src/import_data/leagues/league_table_constructor.py
+++ src/import_data/leagues/league_table_constructor.py
@@ -1,4 +1,4 @@
-from utilities.DB_Connect import DB_Connect
+from utilities.dbconnect import DatabaseConnection
 from utilities.Logger import Logger
 import datetime
 
@@ -17,11 +17,10 @@ def league_table_constructor(driver_logger):
                'PL': 'Players League',
                'UA': 'Union Association',
                'NA': 'National Association'}
-    db, cursor = DB_Connect.grab("baseballData")
+    db = DatabaseConnection()
     for league_id, league_name in leagues.items():
-        DB_Connect.write(db, cursor, 'insert into leagues (leagueId, leagueName) values ("' + league_id + '", "'
-                                     + league_name + '");')
-    DB_Connect.close(db)
+        db.write('insert into leagues (leagueId, leagueName) values ("' + league_id + '", "' + league_name + '");')
+    db.close()
     logger.log('Populating leagues table completed\n\n')
     driver_logger.log('\t\tPopulating leagues table completed')
 
