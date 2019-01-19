92530c865572fad1bc47d820c8fc2c2b89a4deae
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-19 16:11:13 -0500

    Update populate_teams_table.py

--- src/import_data/team_data/populate_teams_table.py
+++ src/import_data/team_data/populate_teams_table.py
@@ -1,4 +1,4 @@
-from utilities.DB_Connect import DB_Connect
+from utilities.dbconnect import DatabaseConnection
 from utilities.translate_team_name import translate_team_name
 from utilities.Logger import Logger
 from utilities.time_converter import time_converter
@@ -16,16 +16,16 @@ def populate_teams_table(year, driver_logger):
     logger.log('Begin populating teams table for ' + str(year) + ' || Timestamp: ' + datetime.datetime.today()\
                .strftime('%Y-%m-%d %H:%M:%S'))
     with open('C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\background\\yearTeams.txt', 'rt') as file:
-        db, cursor = DB_Connect.grab("baseballData")
+        db = DatabaseConnection()
         for line in file:
             if str(year) in line:
                 temp_line = line.split(',')[1:-1]
                 for team in temp_line:
                     team_id = team.split(';')[0]
-                    DB_Connect.write(db, cursor, 'insert into teams (teamId, teamName) values ("' + team_id + '", "'
-                                                 + translate_team_name(team_id).replace("'", "\'") + '");')
+                    db.write('insert into teams (teamId, teamName) values ("' + team_id + '", "'
+                             + translate_team_name(team_id).replace("'", "\'") + '");')
                 break
-    DB_Connect.close(db)
+    db.close()
     total_time = time.time() - start_time
     logger.log('Populating teams table completed: ' + time_converter(total_time))
     driver_logger.log('\t\tTime = ' + time_converter(total_time))
