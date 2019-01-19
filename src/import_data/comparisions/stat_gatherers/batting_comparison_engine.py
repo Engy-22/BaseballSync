c4bbf01631ab3f78c3647e209e0d4a017c1c4d7a
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-19 15:10:43 -0500

    Update batting_comparison_engine.py

--- src/import_data/comparisions/stat_gatherers/batting_comparison_engine.py
+++ src/import_data/comparisions/stat_gatherers/batting_comparison_engine.py
@@ -1,14 +1,11 @@
 from import_data.comparisions.getters.hitting_getters import get_hitter_stats
 from import_data.comparisions.file_writers.write_to_file_players import write_to_file
-from utilities.DB_Connect import DB_Connect
 
 
 def make_hitter_comparisons(hitters, year, possible_comps, year_pa, year_totals, driver_logger):
     comparisons = {}
-    db, cursor = DB_Connect.grab("baseballData")
     for hitter in hitters:
         comparisons[hitter] = determine_comp(hitter, year, possible_comps, year_pa, year_totals, driver_logger)
-    DB_Connect.close(db)
     write_to_file(year, comparisons, "batting")
 
 
