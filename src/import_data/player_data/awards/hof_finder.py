0f1e21c95f7bd2f35a73baa7a44e2b34b064a59b
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-19 15:35:29 -0500

    Update hof_finder.py

--- src/import_data/player_data/awards/hof_finder.py
+++ src/import_data/player_data/awards/hof_finder.py
@@ -2,7 +2,7 @@ import time
 import datetime
 from utilities.time_converter import time_converter
 from utilities.Logger import Logger
-from utilities.DB_Connect import DB_Connect
+from utilities.dbconnect import DatabaseConnection
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
@@ -17,20 +17,19 @@ def hof_finder(driver_logger):
     hof_table = str(BeautifulSoup(urlopen('https://www.baseball-reference.com/awards/hof.shtml'), 'html.parser')).\
         split('<tbody>')[1].split('</tbody>')[0]
     rows = hof_table.split('<tr>')[1:]
-    db, cursor = DB_Connect.grab("baseballData")
+    db = DatabaseConnection()
     for row in rows:
         person = row.split('data-append-csv="')[1].split('"')[0]
         year = row.split('<a href="/awards/hof_')[1].split('.shtml')[0]
         induction_type = row.split('data-stat="category_hof">')[1].split('<')[0]
         if induction_type == 'Player':
-            DB_Connect.write(db, cursor, 'update players set HOF = ' + str(year)
+            db.write('update players set HOF = ' + str(year)
                                          + ' where playerId = "' + person + '";')
         elif induction_type == 'Manager':
-            DB_Connect.write(db, cursor, 'update managers set HOF = ' + str(year)
-                                         + ' where managerId = "' + person + '";')
+            db.write('update managers set HOF = ' + str(year) + ' where managerId = "' + person + '";')
         else:
             continue
-    DB_Connect.close(db)
+    db.close()
     total_time = time_converter(time.time() - start_time)
     logger.log("Done finding hall of famers: time = " + total_time + '\n\n')
     driver_logger.log("\t\tTime = " + total_time)
