5f41617eb9df33a05894d1013cd5b03d33be24b7
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-19 16:10:57 -0500

    Update offensive_comparison_engine.py

--- src/import_data/comparisions/stat_gatherers/offensive_comparison_engine.py
+++ src/import_data/comparisions/stat_gatherers/offensive_comparison_engine.py
@@ -1,14 +1,12 @@
 from import_data.comparisions.getters.offensive_team_getters import get_year_totals, get_offensive_stats
 from import_data.comparisions.file_writers.write_to_file_teams import write_to_file
-from utilities.DB_Connect import DB_Connect
+from utilities.dbconnect import DatabaseConnection
 
 
 def make_offensive_comparisons(ty_uids, year, possible_comps, driver_logger):
     comparisons = {}
-    db, cursor = DB_Connect.grab("baseballData")
     for ty_uid in ty_uids:
         comparisons[ty_uid] = determine_comp(ty_uid, year, possible_comps, driver_logger)
-    DB_Connect.close(db)
     write_to_file(comparisons, "offense")
 
 
