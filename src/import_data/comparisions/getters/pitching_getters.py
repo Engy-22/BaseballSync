f3ee6002dbc298a26378d54019eed854f2f897a3
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-19 15:26:21 -0500

    Update pitching_getters.py

--- src/import_data/comparisions/getters/pitching_getters.py
+++ src/import_data/comparisions/getters/pitching_getters.py
@@ -1,33 +1,31 @@
-from utilities.DB_Connect import DB_Connect
+from utilities.dbconnect import DatabaseConnection
 
 
 def get_pitcher_stats(pitcher, year, driver_logger):
     stats = {'IP': 0, 'H': 0, '2B': 0, '3B': 0, 'HR': 0, 'SO': 0, 'BB': 0, 'ER': 0}
-    db, cursor = DB_Connect.grab("baseballData")
-    pa = int(DB_Connect.read(cursor, 'select player_pitching.pa from player_pitching, player_teams where player_pitchi'
-                                     + 'ng.PT_uniqueidentifier = player_teams.PT_uniqueidentifier and player_teams.'
-                                     + 'playerId = "' + pitcher + '" and year = ' + str(year) + ';')[0][0])
+    db = DatabaseConnection()
+    pa = int(db.read('select player_pitching.pa from player_pitching, player_teams where player_pitching.'
+                     'PT_uniqueidentifier = player_teams.PT_uniqueidentifier and player_teams.playerId = "' + pitcher
+                     + '" and year = ' + str(year) + ';')[0][0])
     for key, value in stats.items():
         try:
-            stats[key] = float(DB_Connect.read(cursor, 'select player_pitching.' + key + ' from player_pitching, '
-                                                       'player_teams where player_pitching.PT_uniqueidentifier = '
-                                                       'player_teams.PT_uniqueidentifier and player_teams.playerId = "'
-                                                       + pitcher + '" and year = ' + str(year) + ';')[0][0])
+            stats[key] = float(db.read('select player_pitching.' + key + ' from player_pitching, player_teams where '
+                                       'player_pitching.PT_uniqueidentifier = player_teams.PT_uniqueidentifier and '
+                                       'player_teams.playerId = "' + pitcher + '" and year = ' + str(year) + ';')[0][0])
         except TypeError:
             pa = 0
             break
-    DB_Connect.close(db)
+    db.close()
     return pa, stats
 
 
 def get_year_totals(year, driver_logger):
     year_totals = {'IP': 0, 'H': 0, '2B': 0, '3B': 0, 'HR': 0, 'SO': 0, 'BB': 0, 'ER': 0}
-    db, cursor = DB_Connect.grab("baseballData")
-    pa = int(DB_Connect.read(cursor, 'select pa from years where year = ' + str(year) + ';')[0][0])
+    db = DatabaseConnection()
+    pa = int(db.read('select pa from years where year = ' + str(year) + ';')[0][0])
     for key, value in year_totals.items():
-        year_totals[key] = float(DB_Connect.read(cursor, 'select ' + key + ' from years where year = ' + str(year)
-                                                         + ';')[0][0])
-    DB_Connect.close(db)
+        year_totals[key] = float(db.read('select ' + key + ' from years where year = ' + str(year) + ';')[0][0])
+    db.close()
     return pa, year_totals
 
 
