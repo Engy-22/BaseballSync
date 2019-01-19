dc048b936ec2d4a623e218b1c76f0952fc523fee
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-19 15:26:28 -0500

    Update hitting_getters.py

--- src/import_data/comparisions/getters/hitting_getters.py
+++ src/import_data/comparisions/getters/hitting_getters.py
@@ -1,28 +1,27 @@
-from utilities.DB_Connect import DB_Connect
+from utilities.dbconnect import DatabaseConnection
 
 
 def get_hitter_stats(hitter, year, driver_logger):
     stats = {'R': 0, 'RBI': 0, 'H': 0, '2B': 0, '3B': 0, 'HR': 0, 'SB': 0, 'BB': 0, 'SO': 0}
-    db, cursor = DB_Connect.grab("baseballData")
-    pa = int(DB_Connect.read(cursor, 'select player_batting.pa from player_batting, player_teams where '
-                                     + 'player_batting.PT_uniqueidentifier = player_teams.PT_uniqueidentifier and '
-                                     + 'player_teams.playerId = "' + hitter + '" and year = ' + str(year) + ';')[0][0])
+    db = DatabaseConnection()
+    pa = int(db.read('select player_batting.pa from player_batting, player_teams where player_batting.'
+                     'PT_uniqueidentifier = player_teams.PT_uniqueidentifier and ' + 'player_teams.playerId = "'
+                     + hitter + '" and year = ' + str(year) + ';')[0][0])
     for key, value in stats.items():
-        stats[key] = int(DB_Connect.read(cursor, 'select player_batting.' + key + ' from player_batting, player_teams'
-                                                 + ' where player_batting.PT_uniqueidentifier ='
-                                                 + ' player_teams.PT_uniqueidentifier and player_teams.playerId = "'
-                                                 + hitter + '" and year = ' + str(year) + ';')[0][0])
-    DB_Connect.close(db)
+        stats[key] = int(db.read('select player_batting.' + key + ' from player_batting, player_teams where '
+                                 'player_batting.PT_uniqueidentifier = player_teams.PT_uniqueidentifier and '
+                                 'player_teams.playerId = "' + hitter + '" and year = ' + str(year) + ';')[0][0])
+    db.close()
     return pa, stats
 
 
 def get_year_totals(year, driver_logger):
     year_totals = {'R': 0, 'RBI': 0, 'H': 0, '2B': 0, '3B': 0, 'HR': 0, 'SB': 0, 'BB': 0, 'SO': 0}
-    db, cursor = DB_Connect.grab("baseballData")
-    pa = int(DB_Connect.read(cursor, 'select pa from years where year = ' + str(year) + ';')[0][0])
+    db = DatabaseConnection()
+    pa = int(db.read('select pa from years where year = ' + str(year) + ';')[0][0])
     for key, value in year_totals.items():
-        year_totals[key] = DB_Connect.read(cursor, 'select ' + key + ' from years where year=' + str(year) + ';')[0][0]
-    DB_Connect.close(db)
+        year_totals[key] = db.read('select ' + key + ' from years where year=' + str(year) + ';')[0][0]
+    db.close()
     return pa, year_totals
 
 
