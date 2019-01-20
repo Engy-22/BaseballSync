fb4c835b5126405935cfddb4adb20f34dfa61f28
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-20 16:41:36 -0500

    Update write_to_file_teams.py

--- src/import_data/comparisions/file_writers/write_to_file_teams.py
+++ src/import_data/comparisions/file_writers/write_to_file_teams.py
@@ -1,22 +1,32 @@
+import os
 from utilities.dbconnect import DatabaseConnection
+from concurrent.futures import ThreadPoolExecutor
 
 
 def write_to_file(comparisons, stat_type):
     db = DatabaseConnection()
-    for ty_uid, comp in comparisons.items():
-        if comp is not None:
-            comp_pull = 1 - float(db.read('select certainty from team_years where ty_uniqueidentifier = ' + str(ty_uid)
-                                          + ';')[0][0])
-            if len(db.read('select comp_id from comparisons_team_' + stat_type + '_overall where ty_uniqueidentifier = '
-                           + str(ty_uid) + ';')) == 0:
-                db.write('insert into comparisons_team_' + stat_type + '_overall (comp_id, ty_uniqueidentifier, comp, '
-                         'comp_pull) values (default, ' + str(ty_uid) + ', ' + str(comp) + ', ' + str(comp_pull) + ');')
+    with ThreadPoolExecutor(os.cpu_count()) as executor:
+        for ty_uid, comp in comparisons.items():
+            if comp is not None:
+                comp_pull = 1 - float(db.read('select certainty from team_years where ty_uniqueidentifier = '
+                                              + str(ty_uid) + ';')[0][0])
+                if len(db.read('select comp_id from comparisons_team_' + stat_type + '_overall where '
+                               'ty_uniqueidentifier = ' + str(ty_uid) + ';')) == 0:
+                    executor.submit(transact, 'insert into comparisons_team_' + stat_type + '_overall (comp_id, '
+                                              'ty_uniqueidentifier, comp, comp_pull) values (default, ' + str(ty_uid)
+                                              + ', ' + str(comp) + ', ' + str(comp_pull) + ');')
+                else:
+                    db.write('update comparisons_team_' + stat_type + '_overall set comp = ' + str(comp)
+                             + ', comp_pull = ' + str(comp_pull) + ' where ty_uniqueidentifier = ' + str(ty_uid) + ';')
             else:
-                db.write('update comparisons_team_' + stat_type + '_overall set comp = ' + str(comp) + ', comp_pull = '
-                         + str(comp_pull) + ' where ty_uniqueidentifier = ' + str(ty_uid) + ';')
-        else:
-            if len(db.read('select comp_id from comparisons_team_' + stat_type + '_overall where ty_uniqueidentifier = '
-                           + str(ty_uid) + ';')) == 0:
-                db.write('insert into comparisons_team_' + stat_type + '_overall (comp_id, ty_uniqueidentifier) values '
-                         '(default, ' + str(ty_uid) + ');')
+                if len(db.read('select comp_id from comparisons_team_' + stat_type + '_overall where '
+                               'ty_uniqueidentifier = ' + str(ty_uid) + ';')) == 0:
+                    executor.submit(transact, 'insert into comparisons_team_' + stat_type + '_overall (comp_id, '
+                                              'ty_uniqueidentifier) values (default, ' + str(ty_uid) + ');')
+    db.close()
+
+
+def transact(statement):
+    db = DatabaseConnection()
+    db.write(statement)
     db.close()
