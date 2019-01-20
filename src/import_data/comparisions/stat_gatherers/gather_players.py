66a6e00f71bf366e3df7b281d358ff7acc2017c5
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-20 15:53:34 -0500

    Update gather_players.py

--- src/import_data/comparisions/stat_gatherers/gather_players.py
+++ src/import_data/comparisions/stat_gatherers/gather_players.py
@@ -1,13 +1,15 @@
 from utilities.dbconnect import DatabaseConnection
 
 
-def gather_players(year, player_type, all, driver_logger):
+def gather_players(year, player_type, all, logger):
     db = DatabaseConnection()
     query = "select playerId from player_teams, player_" + player_type + " where player_" + player_type + ".PT_unique"\
             "identifier = player_teams.PT_uniqueidentifier and player_" + player_type + ".year = " + str(year)
     if all:
+        logger.log('\t\t\tGathering all players')
         player_list = list(db.read(query + ";"))
     else:
+        logger.log('\t\t\tGathering players that need comps')
         player_list = list(db.read(query + " and certainty < 1.0 and certainty > 0.0;"))
     db.close()
     return [player[0] for player in player_list]
