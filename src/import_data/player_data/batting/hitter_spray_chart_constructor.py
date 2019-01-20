13ea8cc16483d3f6ed83a3fa541468b258910bdf
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-20 16:54:21 -0500

    Update hitter_spray_chart_constructor.py

--- src/import_data/player_data/batting/hitter_spray_chart_constructor.py
+++ src/import_data/player_data/batting/hitter_spray_chart_constructor.py
@@ -149,8 +149,8 @@ def write_to_file(year, pt_uid, stat_list1, stat_list2):
         if value == '':
             value = '0'
         query_string += key + ' = ' + value + ', '
-    db.write('update player_batting set ' + query_string[:-2] + ' where PT_uniqueidentifier = '
-                                 + str(pt_uid) + ' and year = ' + str(year) + ';')
+    db.write('update player_batting set ' + query_string[:-2] + ' where PT_uniqueidentifier = ' + str(pt_uid)
+             + ' and year = ' + str(year) + ';')
     db.close()
 
 
@@ -159,5 +159,6 @@ def revisit_bad_gateways(year, data):
         reduce_functionality(year, datum[0])
 
 
-# dump_logger = Logger("C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\logs\\import_data\\dump.log")
-# hitter_spray_chart_constructor(2001, dump_logger)
+dump_logger = Logger("C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\logs\\import_data\\dump.log")
+for year in range(1996, 2009, 1):
+    hitter_spray_chart_constructor(year, dump_logger)
