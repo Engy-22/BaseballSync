9dae77d0ae6a0dd9e076fab3e163995384384b78
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-20 15:53:23 -0500

    Update offensive_team_getters.py

--- src/import_data/comparisions/getters/offensive_team_getters.py
+++ src/import_data/comparisions/getters/offensive_team_getters.py
@@ -1,7 +1,7 @@
 from utilities.dbconnect import DatabaseConnection
 
 
-def get_offensive_stats(ty_uid, driver_logger):
+def get_offensive_stats(ty_uid):
     stats = {'R': 0, 'H': 0, '2B': 0, '3B': 0, 'HR': 0, 'SB': 0, 'BB': 0, 'SO': 0}
     db = DatabaseConnection()
     pa = int(db.read('select pa from team_years where ty_uniqueidentifier=' + str(ty_uid) + ';')[0][0])
@@ -12,12 +12,12 @@ def get_offensive_stats(ty_uid, driver_logger):
     return pa, stats
 
 
-def get_year_totals(year, driver_logger):
+def get_year_totals(year, logger):
+    logger.log('\t\t\tGathering ' + str(year) + ' offensive totals')
     year_totals = {'R': 0, 'H': 0, '2B': 0, '3B': 0, 'HR': 0, 'SB': 0, 'BB': 0, 'SO': 0}
     db = DatabaseConnection()
     pa = int(db.read('select pa from years where year = ' + str(year) + ';')[0][0])
     for key, value in year_totals.items():
-        year_totals[key] = \
-        db.read('select ' + key + ' from years where year=' + str(year) + ';')[0][0]
+        year_totals[key] = db.read('select ' + key + ' from years where year=' + str(year) + ';')[0][0]
     db.close()
     return pa, year_totals
