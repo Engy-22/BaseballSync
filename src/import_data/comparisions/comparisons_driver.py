8f9366af0a3a3f7695e643a6f24e5141bc4125c2
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-20 15:30:52 -0500

    Update comparisons_driver.py

--- src/import_data/comparisions/comparisons_driver.py
+++ src/import_data/comparisions/comparisons_driver.py
@@ -27,6 +27,7 @@ def comparisons_driver(most_recent_year, driver_logger):
     possible_defensive_comps = {}
 
     print('making hitter comparisons (overall)')
+    driver_logger.log('\t\tMaking hitter comparisons (overall)')
     hc_time = time.time()
     logger.log("\tMaking hitter comparisons (overall)")
     for year_to_compare in [year for year in range(most_recent_year, 1997, -1)]:
@@ -44,9 +45,12 @@ def comparisons_driver(most_recent_year, driver_logger):
             continue
         make_hitter_comparisons(gather_players(comp_year, "batting", False, logger), comp_year, possible_hitter_comps,
                                 year_pa, year_totals, logger)
-    logger.log("\t\tTime = " + time_converter(time.time() - hc_time))
+    total_time = time_converter(time.time() - hc_time)
+    logger.log("\t\tTime = " + total_time)
+    driver_logger.log('\t\t\tTime = ' + total_time)
 
     print('making pitcher comparisons (overall)')
+    driver_logger.log('\t\tMaking pitcher comparisons (overall)')
     pc_time = time.time()
     logger.log("\tMaking pitcher comparisons (overall)")
     for year_to_compare in [year for year in range(most_recent_year, 1996, -1)]:
@@ -64,9 +68,12 @@ def comparisons_driver(most_recent_year, driver_logger):
             continue
         make_pitcher_comparisons(gather_players(comp_year, "pitching", False, logger), comp_year,
                                  possible_pitcher_comps, year_pa, year_totals, logger)
-    logger.log("\t\tTime = " + time_converter(time.time() - pc_time))
+    total_time = time_converter(time.time() - pc_time)
+    logger.log("\t\tTime = " + total_time)
+    driver_logger.log('\t\t\tTime = ' + total_time)
 
     print('making team offensive comparisons')
+    driver_logger.log('\t\tMaking team offensive comparisons')
     oc_time = time.time()
     logger.log("\tMaking offensive comparisons (overall)")
     for year_to_compare in [year for year in range(most_recent_year, 1997, -1)]:
@@ -82,9 +89,12 @@ def comparisons_driver(most_recent_year, driver_logger):
             continue
         make_offensive_comparisons(gather_teams(comp_year, logger), possible_offensive_comps, year_pa, year_totals,
                                    logger)
-    logger.log("\t\tTime = " + time_converter(time.time() - oc_time))
+    total_time = time_converter(time.time() - oc_time)
+    logger.log("\t\tTime = " + total_time)
+    driver_logger.log('\t\t\tTime = ' + total_time)
 
     print('making team defensive comparisons')
+    driver_logger.log('\t\tMaking team defensive comparisons')
     dc_time = time.time()
     logger.log("\tMaking defensive comparisons (overall)")
     for year_to_compare in [year for year in range(most_recent_year, 1997, -1)]:
@@ -100,13 +110,15 @@ def comparisons_driver(most_recent_year, driver_logger):
             continue
         make_defensive_comparisons(gather_teams(comp_year, logger), possible_defensive_comps, year_pa, year_totals,
                                    logger)
-    logger.log("\t\tTime = " + time_converter(time.time() - dc_time))
+    total_time = time_converter(time.time() - dc_time)
+    logger.log("\t\tTime = " + total_time)
+    driver_logger.log('\t\t\tTime = ' + total_time)
 
     total_time = time_converter(time.time() - start_time)
     logger.log("Done making comparisons: time = " + total_time + '\n\n')
-    driver_logger.log("\t\tDone making comparisons: time = " + total_time)
+    driver_logger.log("\tDone making comparisons: time = " + total_time)
 
 
-# from utilities.get_most_recent_year import get_most_recent_year
-# dump = Logger("C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\logs\\import_data\\dump.log")
-# comparisons_driver(get_most_recent_year(dump), dump)
+from utilities.get_most_recent_year import get_most_recent_year
+dump = Logger("C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\logs\\import_data\\dump.log")
+comparisons_driver(get_most_recent_year(dump), dump)
