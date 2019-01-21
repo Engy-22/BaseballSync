9d6fb5e706a851e71d507d7d2379e27e4f8e358e
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-21 12:31:11 -0500

    Update driver.py

--- src/import_data/driver.py
+++ src/import_data/driver.py
@@ -59,7 +59,7 @@ def driver(year, driver_log):
     hitter_spray_chart_constructor(year, driver_log)
     pitcher_spray_chart_constructor(year, driver_log)
     team_certainties(year, driver_log)
-    rank_driver(year, driver_log)
+    # rank_driver(year, driver_log)
     award_winner_driver(year, driver_log)
     driver_log.log('Time taken to download ' + str(year) + ' data: ' + time_converter(time.time() - driver_time) + '\n')
 
@@ -75,8 +75,8 @@ if __name__ == '__main__':
     for year in range(1996, 1999, 1):
         years.append(year)
         driver(year, main_logger)
-    if most_recent_year > 1997:
-        comparisons_driver(most_recent_year, main_logger)
+    rank_driver(years[-1], main_logger)
+    comparisons_driver(years[-1], main_logger)
     hof_finder(main_logger)
     main_logger.log('Driver complete for year' + stringify_list(years) + ': time = '
                     + time_converter(time.time() - start_time) + '\n\n\n')
