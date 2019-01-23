50fb80554639988182812de8d343a397cdbf264e
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-23 16:50:28 -0500

    Update write_to_file.py

--- src/import_data/player_data/pitch_fx/write_to_file.py
+++ src/import_data/player_data/pitch_fx/write_to_file.py
@@ -1,7 +1,11 @@
 from utilities.dbconnect import DatabaseConnection
 
 
-def write_to_file(pitch_type, swing_take, meta_data):
+def write_to_file(player_type, player, year, matchup, count, pitch_type, ball_strike, swing_take, outcome, trajectory,
+                  field, direction):
     db = DatabaseConnection()
-    db.write()
+    db.write('insert into ' + player_type + '_pitches (pitchid, playerid, year, matchup, count, pitch_type, swing_take,'
+             ' ball_strike, outcome, trajectory, field, direction) values (default, ' + str(player) + ', ' + str(year)
+             + ', "' + matchup + '", "' + str(count) + '", "' + pitch_type + '", "' + swing_take + '", "' + ball_strike
+             + '", "' + outcome + '", "' + trajectory + '", "' + field + '", "' + direction + '");')
     db.close()
