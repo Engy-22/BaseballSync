dd440239961dd64155419fe5290832a34bf36efe
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-23 16:50:22 -0500

    Create determine_trajectory.py

---
+++ src/import_data/player_data/pitch_fx/translators/determine_trajectory.py
@@ -0,0 +1,52 @@
+def determine_trajectory(event, description):
+    outcomes = {'so': 'none', 'go': 'gb', 'fo': 'fb', 'lo': 'ld', 'po': 'fb', 'bb': 'none', 'hbp': 'none', 'sh': 'gb',
+                'ibb': 'none', 'gdp': 'gb', 'sf': 'fb'}
+    try:
+        trajectory = outcomes[event]
+    except KeyError:
+        outcomes = {'line': 'ld', 'fly': 'fb', 'ground': 'gb', 'pop': 'fb'}
+        if event in ['1b', '2b', '3b', 'hr']:
+            try:
+                trajectory = outcomes[description.split('on a ')[1].split(' ')[0]]
+            except KeyError:
+                trajectory = outcomes[description.split('on a ')[1].split(' ')[1]]
+        elif event == 'fc':
+            if description.split(' into ')[0].split(' ')[-1] == 'grounds':
+                trajectory = 'gb'
+            elif description.split(' into ')[0].split(' ')[-1] == 'flies':
+                trajectory = 'fb'
+            elif description.split(' into ')[0].split(' ')[-2] == 'ground':
+                trajectory = 'gb'
+            elif 'reaches' in description:
+                fielders = {'first': 'gb', 'second': 'gb', 'third': 'gb', 'shortstop': 'gb', 'left': 'fb',
+                            'right': 'fb', 'center': 'fb'}
+                try:
+                    trajectory = fielders[description.split('fielded by ')[1].split(' ')[0]]
+                except IndexError:
+                    trajectory = fielders[description.split(', ')[1].split(' ')[0]]
+            else:
+                print(event)
+                print(description)
+        elif event == 'dp':
+            if description.split(' into ')[0].split(' ')[-1] == 'lines':
+                trajectory = 'ld'
+            elif description.split(' into ')[0].split(' ')[-1] == 'flies':
+                trajectory = 'fb'
+            elif description.split(' into ')[0].split(' ')[-1] == 'pops':
+                trajectory = 'fb'
+            else:
+                print(event)
+                print(description)
+                print(description.split(' into ')[0].split(' ')[-1])
+        elif event == 'error':
+            error = event.split('by')[1].split(' ')[0]
+            if error in ['left', 'center', 'right']:
+                trajectory = 'fb'
+            else:
+                trajectory = 'gb'
+        else:
+            if event != 'cs':
+                print(event)
+                print(description)
+            trajectory = "none"
+    return trajectory
