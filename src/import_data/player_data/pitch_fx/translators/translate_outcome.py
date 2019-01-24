025c32756a1f1d334eae57f080042ab03f27fe11
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-24 15:46:33 -0500

    Update translate_outcome.py

--- src/import_data/player_data/pitch_fx/translators/translate_outcome.py
+++ src/import_data/player_data/pitch_fx/translators/translate_outcome.py
@@ -2,9 +2,9 @@ def translate_pitch_outcome(outcome, description):
     outcomes = {'Single': '1b', 'Double': '2b', 'Triple': '3b', 'Strikeout': 'so', 'Home Run': 'hr', 'Groundout': 'go',
                 'Flyout': 'fo', 'Lineout': 'lo', 'Pop Out': 'po', 'Walk': 'bb', 'Hit By Pitch': 'hbp',
                 'Sac Bunt': 'sh', 'Intent Walk': 'ibb', 'Forceout': 'fc', 'Grounded Into DP': 'gdp',
-                'Double Play': 'dp', 'Field Error': 'error', 'Runner Out': 'cs', 'Sac Fly': 'sf',
+                'Double Play': 'dp', 'Triple Play': '3p', 'Field Error': 'error', 'Runner Out': 'cs', 'Sac Fly': 'sf',
                 'Bunt Groundout': 'go', 'Fielders Choice': 'fc', 'Fielders Choice Out': 'fc', 'Sac Fly DP': 'dp',
-                'Bunt Pop Out': 'po', 'Bunt Lineout': 'po', 'Strikeout - DP': 'so'}
+                'Bunt Pop Out': 'po', 'Bunt Lineout': 'po', 'Strikeout - DP': 'so', 'Catcher Interference': 'error'}
     try:
         this_outcome = outcomes[outcome]
     except KeyError:
@@ -18,9 +18,17 @@ def translate_pitch_outcome(outcome, description):
             else:
                 this_outcome = 'hr'
         elif outcome == 'Batter Interference':
-            this_outcome = 'none'
-        elif outcome == 'Catcher Interference':
-            this_outcome = 'error'
+            if 'grounds out' in description:
+                this_outcome = 'go'
+            elif 'strikes out' in description:
+                this_outcome = 'so'
+            elif 'flies out' in description:
+                this_outcome = 'fo'
+            elif 'lines out' in description:
+                this_outcome = 'lo'
+            else:
+                print('\n\n' + outcome)
+                this_outcome = "none"
         else:
             print('\n\n' + outcome)
             this_outcome = "none"
