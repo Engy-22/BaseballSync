fbaf7bdc93fca0e09c2fc84084103ac45e646b61
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-24 09:56:47 -0500

    Update write_to_file.py

--- src/import_data/player_data/pitch_fx/write_to_file.py
+++ src/import_data/player_data/pitch_fx/write_to_file.py
@@ -1,11 +1,14 @@
 from utilities.dbconnect import DatabaseConnection
 
 
-def write_to_file(player_type, player, year, matchup, count, pitch_type, ball_strike, swing_take, outcome, trajectory,
-                  field, direction):
+def write_to_file(player_type, player_id, team_id, year, matchup, count, pitch_type, ball_strike, swing_take, outcome,
+                  trajectory, field, direction):
     db = DatabaseConnection()
     db.write('insert into ' + player_type + '_pitches (pitchid, playerid, year, matchup, count, pitch_type, swing_take,'
-             ' ball_strike, outcome, trajectory, field, direction) values (default, ' + str(player) + ', ' + str(year)
-             + ', "' + matchup + '", "' + str(count) + '", "' + pitch_type + '", "' + swing_take + '", "' + ball_strike
-             + '", "' + outcome + '", "' + trajectory + '", "' + field + '", "' + direction + '");')
+             ' ball_strike, outcome, trajectory, field, direction, P' + player_type[0] + '_uniqueidentifier) values '
+             '(default, ' + str(player_id) + ', ' + str(year) + ', "' + matchup + '", "' + str(count) + '", "'
+             + pitch_type + '", "' + swing_take + '", "' + ball_strike + '", "' + outcome + '", "' + trajectory + '", "'
+             + field + '", "' + direction + '", (select P' + player_type[0] + '_uniqueidentifier from player_'
+             + player_type + ' where year = ' + str(year) + ' and pt_uniqueidentifier = (select pt_uniqueidentifier '
+             'from player_teams where playerid = "' + player_id + '" and teamid = "' + team_id + '")));')
     db.close()
