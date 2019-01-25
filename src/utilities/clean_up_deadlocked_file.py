a2a73572b8cae9dc4ca599ebd74afedc7f1ab7a3
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-25 00:45:52 -0500

    Update clean_up_deadlocked_file.py

--- src/utilities/clean_up_deadlocked_file.py
+++ src/utilities/clean_up_deadlocked_file.py
@@ -8,3 +8,5 @@ def clean_up_deadlocked_file(driver_logger):
         for line in f:
             db.write(line)
         db.close()
+    file = open('C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\src\\utilities\\deadlocked.txt',
+                'w').close()
