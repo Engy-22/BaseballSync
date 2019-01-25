6d4f070663da42ee7193db2da4b492a171bdcc46
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-25 11:26:10 -0500

    Update determine_trajectory.py

--- src/import_data/player_data/pitch_fx/translators/determine_trajectory.py
+++ src/import_data/player_data/pitch_fx/translators/determine_trajectory.py
@@ -41,6 +41,8 @@ def determine_trajectory(event, description):
                 trajectory = 'fb'
             elif description.split(' into ')[0].split(' ')[-1] == 'grounds':
                 trajectory = 'gb'
+            elif description.split(' into ')[0].split(' ')[-1] == 'bunts':
+                trajectory = 'gb'
             elif "fielder's choice" in description:
                 trajectory = 'gb'
             elif 'challenged' in description:
