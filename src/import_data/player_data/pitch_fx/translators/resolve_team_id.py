03b4ed027ccdcaf80c40f00d007f1af03a0e9793
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-25 13:39:35 -0500

    Update resolve_team_id.py

--- src/import_data/player_data/pitch_fx/translators/resolve_team_id.py
+++ src/import_data/player_data/pitch_fx/translators/resolve_team_id.py
@@ -2,10 +2,11 @@ import csv
 
 
 def resolve_team_id(team_abv, year):
-    with open('C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\background\\pitch_fx_team_finders\\'
-              'pitch_fx_team_finders-' + str(year) + '.csv', 'r') as file:
-        rows = csv.reader(file)
-        for row in rows:
-            if row[0] == team_abv:
-                return row[1]
+    if team_abv not in ['aas', 'nas']:
+        with open('C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\background\\pitch_fx_team_finders\\'
+                  'pitch_fx_team_finders-' + str(year) + '.csv', 'r') as file:
+            rows = csv.reader(file)
+            for row in rows:
+                if row[0] == team_abv:
+                    return row[1]
     return None
