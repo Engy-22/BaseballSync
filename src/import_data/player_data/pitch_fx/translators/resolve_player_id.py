0644756ab0dd25c119e5d3b3152115512998138a
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-25 11:07:49 -0500

    Update resolve_player_id.py

--- src/import_data/player_data/pitch_fx/translators/resolve_player_id.py
+++ src/import_data/player_data/pitch_fx/translators/resolve_player_id.py
@@ -1,6 +1,20 @@
 from utilities.dbconnect import DatabaseConnection
+from xml.dom import minidom
 
 
-def resolve_player_id(player_num, player_name):
+def resolve_player_id(player_num):
+    players_file = minidom.parse('C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\src\\import_data\\'
+                                 'player_data\\pitch_fx\\xml\\players.xml')
+    for ent in players_file.getElementsByTagName('player'):
+        if ent.getAttribute('id') == str(player_num):
+            last_name = ent.getAttribute('last')
+            first_name = ent.getAttribute('first')
+            break
     db = DatabaseConnection()
+    pid = db.read('select playerid from players where lastName = ' + last_name + ' and firstName = ' + first_name + ';')
+    if len(pid) == 1:
+        player_id = pid[0][0]
+    else:
+        player_id = None
     db.close()
+    return player_id
