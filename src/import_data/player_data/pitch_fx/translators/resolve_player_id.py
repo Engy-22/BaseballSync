770e2899171fbf03a7d039679be9b01b73feb177
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-25 10:14:53 -0500

    Update resolve_player_id.py

--- src/import_data/player_data/pitch_fx/translators/resolve_player_id.py
+++ src/import_data/player_data/pitch_fx/translators/resolve_player_id.py
@@ -1,9 +1,6 @@
-import csv
+from utilities.dbconnect import DatabaseConnection
 
 
-def resolve_player_id(player_num):
-    with open('C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\background\\master.csv', 'r') as csv_file:
-        reader = csv.reader(csv_file)
-        for row in reader:
-            if str(player_num) == row[0]:
-                return row[9]
+def resolve_player_id(player_num, player_name):
+    db = DatabaseConnection()
+    db.close()
