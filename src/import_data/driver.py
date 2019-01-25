fc8c3bb8e073b9d373455fd284c7b5ae71957de7
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-25 00:45:55 -0500

    Update driver.py

--- src/import_data/driver.py
+++ src/import_data/driver.py
@@ -73,7 +73,7 @@ if __name__ == '__main__':
     league_table_constructor(main_logger)
     manager_table_constructor(main_logger)
     years = []
-    for year in range(2017, 2019, 1):
+    for year in range(2010, 2017, 1):
         years.append(year)
         driver(year, main_logger)
     rank_driver(years[-1], main_logger)
