938b24be3fa71a1142b5b9e0c411a902a31bf087
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-26 12:26:09 -0500

    Update write_to_file.py

--- src/import_data/player_data/pitch_fx/write_to_file.py
+++ src/import_data/player_data/pitch_fx/write_to_file.py
@@ -19,7 +19,7 @@ def write_to_file(player_type, player_id, team_id, year, matchup, count, pitch_t
         db = DatabaseConnection()
         db.write('insert into ' + player_type + '_pitches (pitchid, playerid, year, matchup, count, pitch_type, '
                  'swing_take, ball_strike, outcome, trajectory, field, direction, P' + player_type[0]
-                 + '_uniqueidentifier) values (default, ' + player_id + ', ' + str(year) + ', "' + matchup + '", "'
+                 + '_uniqueidentifier) values (default, "' + player_id + '", ' + str(year) + ', "' + matchup + '", "'
                  + str(count) + '", "' + pitch_type + '", "' + swing_take + '", "' + ball_strike + '", "' + outcome
                  + '", "' + trajectory + '", "' + field + '", "' + direction + '", (select P' + player_type[0]
                  + '_uniqueidentifier from player_' + player_type[:-2] + 'ing where year = ' + str(year) + ' and '
