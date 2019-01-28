e458c615cac05e6a0e1644ad0216524a2c469a34
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-28 08:49:58 -0500

    Update determine_field.py

--- src/import_data/player_data/pitch_fx/translators/determine_field.py
+++ src/import_data/player_data/pitch_fx/translators/determine_field.py
@@ -2,7 +2,6 @@ def determine_field(event):
     outcomes = {'so': 'none', 'hr': 'of', 'go': 'if', 'fo': 'of', 'po': 'if', 'bb': 'none', 'hbp': 'none', 'sh': 'if',
                 'ibb': 'none', 'gdp': 'if', 'sf': 'of'}
     try:
-        field = outcomes[event]
+        return outcomes[event]
     except KeyError:
-        field = "none"
-    return field
+        return "none"
