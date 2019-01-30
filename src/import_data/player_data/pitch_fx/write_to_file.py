d13589d56cc0f3c2a1a3163d7da959ceabaeb3bb
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-30 10:48:03 -0500

    Update write_to_file.py

--- src/import_data/player_data/pitch_fx/write_to_file.py
+++ src/import_data/player_data/pitch_fx/write_to_file.py
@@ -29,16 +29,17 @@ def write_to_file(player_type, player_id, team_id, year, matchup, count, pitch_t
 
 
 def write_pickoff(pitcher, team, year, base, attempts_successes):
+    intermediate_a_s = '_' + attempts_successes if 'Error' not in base else ''
     db = DatabaseConnection()
     pt_uid = db.read('select pt_uniqueidentifier from player_teams where playerid = "' + pitcher + '" and teamid = "'
                      + team + '";')[0][0]
-    if db.read('select pickoff_' + base + '_' + attempts_successes + ' from player_pitching where pt_uniqueidentifier = '
+    if db.read('select pickoff_' + base + intermediate_a_s + ' from player_pitching where pt_uniqueidentifier = '
                + str(pt_uid) + ' and year = ' + str(year) + ';')[0][0] is None:
-        db.write('update player_pitching set pickoff_' + base + '_' + attempts_successes + ' = 1 where '
-                 'pt_uniqueidentifier = ' + str(pt_uid) + ' and year = ' + str(year) + ';')
+        db.write('update player_pitching set pickoff_' + base + intermediate_a_s + ' = 1 where pt_uniqueidentifier = '
+                 + str(pt_uid) + ' and year = ' + str(year) + ';')
     else:
         db.write('update player_pitching set pickoff_' + base + '_attempts = ' + str(int(db.read('select pickoff_'
-                 + base + '_' + attempts_successes + ' from player_pitching where pt_uniqueidentifier = ' + str(pt_uid)
+                 + base + intermediate_a_s + ' from player_pitching where pt_uniqueidentifier = ' + str(pt_uid)
                  + ' and year = ' + str(year) + ';')[0][0]) + 1) + ' where pt_uniqueidentifier = ' + str(pt_uid)
                  + ' and year = ' + str(year) + ';')
     db.close()
