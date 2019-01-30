b294dfa6b4e2d879f761411af8dd91b12de9e9d1
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-29 22:43:53 -0500

    Create get_pitcher_on_pickoff.py

---
+++ src/import_data/player_data/pitch_fx/translators/get_pitcher_on_pickoff.py
@@ -0,0 +1,5 @@
+def get_pitcher_on_pickoff(pickoff_attempt, xml_file):
+    at_bats = []
+    with open(xml_file, 'r') as file:
+        for at_bat in file.read().split('<at bat'):
+            at_bats.append(at_bat)
