3122297277ab92b039b311e3b03203e1bdefc2df
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-29 23:04:18 -0500

    Update get_pitcher_on_pickoff.py

--- src/import_data/player_data/pitch_fx/translators/get_pitcher_on_pickoff.py
+++ src/import_data/player_data/pitch_fx/translators/get_pitcher_on_pickoff.py
@@ -1,4 +1,4 @@
-def get_pitcher_on_pickoff(pickoff_attempt, xml_file):
+def get_pitcher_on_pickoff_success(pickoff_attempt, xml_file):
     at_bats = []
     with open(xml_file, 'r') as file:
         for at_bat in file.read().split('<at bat'):
