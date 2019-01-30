7633283838386ca27599ea51036ebb19cef0c5b7
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-30 09:35:30 -0500

    Delete get_pitcher_on_pickoff.py

--- src/import_data/player_data/pitch_fx/translators/get_pitcher_on_pickoff.py
+++
@@ -1,5 +0,0 @@
-def get_pitcher_on_pickoff_success(pickoff_attempt, xml_file):
-    at_bats = []
-    with open(xml_file, 'r') as file:
-        for at_bat in file.read().split('<at bat'):
-            at_bats.append(at_bat)
