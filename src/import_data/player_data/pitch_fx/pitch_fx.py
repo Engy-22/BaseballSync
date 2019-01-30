807c900bd6cdd01da22624d42a781ebc566a6a90
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-30 15:42:11 -0500

    Update pitch_fx.py

--- src/import_data/player_data/pitch_fx/pitch_fx.py
+++ src/import_data/player_data/pitch_fx/pitch_fx.py
@@ -8,7 +8,7 @@ from concurrent.futures import ThreadPoolExecutor
 from urllib.request import urlopen, urlretrieve
 from bs4 import BeautifulSoup
 from xml.dom import minidom
-from import_data.player_data.pitch_fx.write_to_file import write_to_file
+from import_data.player_data.pitch_fx.write_to_file import write_to_file, write_pickoff
 from import_data.player_data.pitch_fx.translators.translate_outcome import translate_pitch_outcome
 from import_data.player_data.pitch_fx.translators.determine_swing_take import determine_swing_or_take
 from import_data.player_data.pitch_fx.translators.translate_pitch_type import translate_pitch_type
@@ -17,6 +17,7 @@ from import_data.player_data.pitch_fx.translators.determine_field import determi
 from import_data.player_data.pitch_fx.translators.determine_direction import determine_direction
 from import_data.player_data.pitch_fx.translators.resolve_player_id import resolve_player_id
 from import_data.player_data.pitch_fx.translators.resolve_team_id import resolve_team_id
+from import_data.player_data.pitch_fx.translators.find_pickoff_successes import find_pickoff_successes
 
 innings = {}
 strikes = 0
@@ -37,21 +38,21 @@ def get_pitch_fx_data(year, driver_logger):
     opening_day = db.read('select opening_day from years where year = ' + str(year) + ';')[0][0]
     db.close()
     for month in range(3, 12, 1):
-        # if month > 6:
-        if month >= int(opening_day.split('-')[0]):
-            for day in range(1, 32, 1):
-                # if day > 29:
-                if month == int(opening_day.split('-')[0]) and int(day) < int(opening_day.split('-')[1]):
-                    continue
-                if len(str(day)) == 1:
-                    this_day = '0' + str(day)
-                else:
-                    this_day = str(day)
-                if len(str(month)) == 1:
-                    this_month = '0' + str(month)
-                else:
-                    this_month = str(month)
-                get_day_data(this_day, this_month, str(year))
+        if month > 8:
+            if month >= int(opening_day.split('-')[0]):
+                for day in range(1, 32, 1):
+                    if day > 15:
+                        if month == int(opening_day.split('-')[0]) and int(day) < int(opening_day.split('-')[1]):
+                            continue
+                        if len(str(day)) == 1:
+                            this_day = '0' + str(day)
+                        else:
+                            this_day = str(day)
+                        if len(str(month)) == 1:
+                            this_month = '0' + str(month)
+                        else:
+                            this_month = str(month)
+                        get_day_data(this_day, this_month, str(year))
     total_time = time_converter(time.time() - start_time)
     logger.log("Done fetching " + str(year) + " pitch fx data: time = " + total_time + '\n\n\n\n')
     driver_logger.log("\t\tTime = " + total_time)
@@ -61,7 +62,7 @@ def get_day_data(day, month, year):
     logger.log("\tDownloading data for " + month + '-' + day + '-' + year)
     day_time = time.time()
     home_page_url = 'http://gd2.mlb.com/components/game/mlb/year_' + year + '/month_' + month + '/day_' + day
-    a=0
+    a = 0
     home_page = str(BeautifulSoup(urlopen(home_page_url), 'html.parser')).split('<li>')
     for line in home_page:
         try:
@@ -72,8 +73,6 @@ def get_day_data(day, month, year):
                 players_url = home_page_url[:-6] + line.split('<a href="')[1].split('">')[0] + 'players.xml'
                 logger.log("\t\tDownloading data for game: " + line.split('gid_')[1].split('_')[3] + '_'
                            + line.split('gid_')[1].split('_')[4] + ' - ' + innings_url)
-                print("\nDownloading data for game: " + line.split('gid_')[1].split('_')[3] + '_' + line.split('gid_')[1]
-                      .split('_')[4])
                 try:
                     innings_page = str(BeautifulSoup(urlopen(innings_url), 'html.parser')).split('<li>')
                     urlretrieve(players_url, 'C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\src\\'
@@ -85,7 +84,6 @@ def get_day_data(day, month, year):
                         try:
                             if inning.split('<a href="inning_')[1].split('.')[0].isdigit():
                                 individual_inning_url = inning.split('.xml"> ')[1].split('</a>')[0]
-                                # print(innings_url + individual_inning_url)
                                 executor.submit(load_xml, innings_url + individual_inning_url,
                                                 individual_inning_url.split('_')[1].split('.xml')[0])
                         except IndexError:
@@ -93,13 +91,13 @@ def get_day_data(day, month, year):
                 parse_innings(year)
                 clear_xmls()
         except IndexError as e:
-            a+=1
-            if a==3:
+            a += 1
+            if a == 3:
                 raise e
             clear_xmls()
             continue
         except KeyError as e:
-            print(e)
+            raise e
             clear_xmls()
             continue
     logger.log("\tDone downloading data for " + month + '-' + day + '-' + year + ": time = "
@@ -127,26 +125,27 @@ def parse_innings(year):
         doc = minidom.parse(dir + '\\players.xml')
         away_team = resolve_team_id(doc.getElementsByTagName('team')[0].getAttribute('id'))
         home_team = resolve_team_id(doc.getElementsByTagName('team')[1].getAttribute('id'))
-        print(home_team)
-        print(away_team)
+        if None in [away_team, home_team]:
+            raise Exception('None team')
         for xml_file in os.listdir(dir):
             if 'players' not in xml_file:
                 parse_inning(year, dir + '\\' + xml_file, away_team, home_team)
-    except FileNotFoundError:
+    except FileNotFoundError as e:
         pass
 
 
 def parse_inning(year, xml_file, away_team, home_team):
-    # print(xml_file)
     doc = minidom.parse(xml_file)
     away_at_bats = doc.getElementsByTagName('inning')[0].getElementsByTagName('top')[0].getElementsByTagName('atbat')
     for at_bat in away_at_bats:
         parse_at_bat(year, at_bat, home_team, away_team)
+    parse_pickoff_success(year, home_team, 'top', xml_file)
     try:
         home_at_bats = doc.getElementsByTagName('inning')[0].getElementsByTagName('bottom')[0]\
             .getElementsByTagName('atbat')
         for at_bat in home_at_bats:
             parse_at_bat(year, at_bat, away_team, home_team)
+        parse_pickoff_success(year, away_team, 'bottom', xml_file)
     except IndexError:
         pass
 
@@ -160,8 +159,8 @@ def parse_at_bat(year, at_bat, pitcher_team, hitter_team):
                  'batter_team': hitter_team,
                  'temp_outcome': at_bat.getAttribute('event'),
                  'ab_description': at_bat.getAttribute('des'),
-                 'batter_orientation': 'v' + at_bat.getAttribute('stand="').lower() + 'hb',
-                 'pitcher_orientation': 'v' + at_bat.getAttribute('p_throws="').lower() + 'hp'}
+                 'batter_orientation': 'v' + at_bat.getAttribute('stand').lower() + 'hb',
+                 'pitcher_orientation': 'v' + at_bat.getAttribute('p_throws').lower() + 'hp'}
     pitches = at_bat.getElementsByTagName('pitch')
     global strikes
     strikes = 0
@@ -169,9 +168,9 @@ def parse_at_bat(year, at_bat, pitcher_team, hitter_team):
     balls = 0
     for pitch in pitches:
         parse_pitch(year, pitch, meta_data, pitches.index(pitch)+1 == len(pitches))
-    actions = at_bat.getElementsByTagName('action')
-    if len(actions) > 0:
-        print(actions)
+    pickoff_attempts = at_bat.getElementsByTagName('po')
+    for pickoff_attempt in pickoff_attempts:
+        parse_pickoff_attempt(pickoff_attempt, meta_data['pitcher_id'], pitcher_team, year)
 
 
 def parse_pitch(year, pitch, meta_data, last_pitch):
@@ -188,8 +187,8 @@ def parse_pitch(year, pitch, meta_data, last_pitch):
     if last_pitch:
         outcome = translate_pitch_outcome(meta_data['temp_outcome'], meta_data['ab_description'])
         trajectory = determine_trajectory(outcome, meta_data['ab_description'])
-        field = determine_field(outcome)
-        direction = determine_direction(meta_data['ab_description'], meta_data['batter_orientation'])
+        field = determine_field(outcome, meta_data['ab_description'])
+        direction = determine_direction(meta_data['ab_description'], meta_data['batter_orientation'][1])
     else:
         outcome = "none"
         trajectory = "none"
@@ -207,6 +206,25 @@ def parse_pitch(year, pitch, meta_data, last_pitch):
                              trajectory, field, direction, meta_data['original_batter_id'])
     except KeyError:
         pass
+
+
+def parse_pickoff_attempt(pickoff_attempt, pitcher, team, year):
+    try:
+        write_pickoff(pitcher, team, year, pickoff_attempt.getAttribute('des').split('Pickoff Error ')[1], 'error')
+    except IndexError:
+        try:
+            write_pickoff(pitcher, team, year, pickoff_attempt.getAttribute('des').split('Pickoff Attempt ')[1],
+                          'attempts')
+        except IndexError:
+            pass
+
+
+def parse_pickoff_success(year, team, top_bottom, xml):
+    successes = find_pickoff_successes(top_bottom, year, team, xml)
+    for pitcher, success in successes.items():
+        for base in success:
+            if 'Error' not in base:
+                write_pickoff(pitcher, team, year, base, 'successes')
 
 
 for year in range(2018, 2019, 1):
