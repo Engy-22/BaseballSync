5452701769c551e5b8641c694a8b022f1091bb79
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-30 08:42:59 -0500

    Update write_to_file.py

--- src/import_data/player_data/pitch_fx/write_to_file.py
+++ src/import_data/player_data/pitch_fx/write_to_file.py
@@ -30,7 +30,7 @@ def write_to_file(player_type, player_id, team_id, year, matchup, count, pitch_t
 
 def write_pickoff_attempt(pitcher, team, year, base):
     db = DatabaseConnection()
-    pt_uid = db.read('select pt_uniqueidentifier from player_teams where playerid = "' + pitcher + '" + and teamid = "'
+    pt_uid = db.read('select pt_uniqueidentifier from player_teams where playerid = "' + pitcher + '" and teamid = "'
                      + team + '";')[0][0]
     if len(db.read('select pickoff_' + base + '_attempts from player_pitching where pt_uniqueidentifier = '
                    + pt_uid + ' and year = ' + str(year) + ';')) == 0:
