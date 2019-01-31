1ce8cf2f882cf404aacb4f25ddcd7b37a129ea1e
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-31 15:08:47 -0500

    Update plate_appearance.py

--- src/controller/plate_appearance.py
+++ src/controller/plate_appearance.py
@@ -1,2 +1,8 @@
+from model.PlateAppearance import PlateAppearance
+from controller.pitch import simulate_pitch
+
+
 def simulate_plate_appearance(batter_id, pitcher_id):
-    pass
+    plate_appearance = PlateAppearance(batter_id, pitcher_id)
+    while plate_appearance.get_balls() < 4 and plate_appearance.get_strikes() < 3 and True:  # True = hit, hbp, out, error, etc.
+        simulate_pitch(plate_appearance.get_balls(), plate_appearance.get_strikes())
