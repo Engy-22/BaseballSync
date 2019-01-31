f26aabfedb447f8defe96a72ad964ceefcc97e3c
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-31 16:34:23 -0500

    Update primary_and_secondary_positions.py

--- src/import_data/player_data/fielding/primary_and_secondary_positions.py
+++ src/import_data/player_data/fielding/primary_and_secondary_positions.py
@@ -1,6 +1,6 @@
 import time
 import datetime
-from utilities.dbconnect import DatabaseConnection
+from utilities.baseball_data_connection import DatabaseConnection
 from utilities.logger import Logger
 from utilities.time_converter import time_converter
 
