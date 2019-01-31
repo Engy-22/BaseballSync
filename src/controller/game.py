067be16284ad465be3f2c76d86e41e57ee691027
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-31 14:46:08 -0500

    Update game.py

--- src/controller/game.py
+++ src/controller/game.py
@@ -1,11 +1,8 @@
 from model.Game import Game
-from controller.inning import inning
+from controller.inning import simulate_inning
 
 
 def simulate_game(away_team, home_team):
     game = Game(away_team, home_team)
     while game.get_inning() <= 9 or game.away_score == game.home_score:
-        if game.get_inning() == 9 and game.get_half_inning() == 'bottom' and game.home_score > game.away_score:
-            break
-        else:
-            inning(game)
+        simulate_inning(game)
