e0889fbb52e8b44bc9b7fe570f43a90715a015c2
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-31 14:51:06 -0500

    Update game.py

--- src/controller/game.py
+++ src/controller/game.py
@@ -6,3 +6,4 @@ def simulate_game(away_team, home_team):
     game = Game(away_team, home_team)
     while game.get_inning() <= 9 or game.away_score == game.home_score:
         simulate_inning(game)
+        game.increment_inning()
