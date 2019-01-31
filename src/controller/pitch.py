13bfdce0311ac674a8188367f0ddea143efff261
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-31 16:18:41 -0500

    Update pitch.py

--- src/controller/pitch.py
+++ src/controller/pitch.py
@@ -1,7 +1,12 @@
 import random
+from utilities.logger import Logger
+
+logger = Logger("C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\logs\\controller\\pitch.log")
 
 
-def simulate_pitch(balls, strikes):
+def simulate_pitch(balls, strikes, driver_logger):
+    driver_logger.log('Simulating ' + str(balls + '-' + str(strikes)) + ' pitch')
+    logger.log('Simulating ' + str(balls + '-' + str(strikes)) + ' pitch')
     pitch_data = {}
     pitch_data['pitch_type'] = ''
     pitch_data['ball_strike'] = ''
