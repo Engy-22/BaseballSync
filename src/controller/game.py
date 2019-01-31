f721dc4696462566dc8272169dee1ef29f5a7310
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-31 16:13:10 -0500

    Update game.py

--- src/controller/game.py
+++ src/controller/game.py
@@ -1,9 +1,20 @@
+from utilities.logger import Logger
 from model.Game import Game
 from controller.inning import simulate_inning
+import time
+from utilities.time_converter import time_converter
+
+logger = Logger("C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\logs\\controller\\game.log")
 
 
-def simulate_game(away_team, home_team):
+def simulate_game(away_team, home_team, driver_logger):
+    driver_logger.log("\tStarting game simulation: " + away_team + " @ " + home_team)
+    start_time = time.time()
+    logger.log("Starting game simulation: " + away_team + " @ " + home_team)
     game = Game(away_team, home_team)
     while game.get_inning() <= 9 or game.away_score == game.home_score:
-        simulate_inning(game)
+        simulate_inning(game, logger)
         game.increment_inning()
+    total_time = time_converter(time.time() - start_time)
+    logger.log("Done simulating game: " + away_team + " @ " + home_team + ": Time = " + total_time + '\n\n')
+    driver_logger.log("Done simulating game: " + away_team + " @ " + home_team + ": Time = " + total_time)
