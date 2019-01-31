1db5dcf7df26d92c4260bc6b91cb954ac5b0ca1e
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-31 14:46:11 -0500

    Update inning.py

--- src/controller/inning.py
+++ src/controller/inning.py
@@ -1,2 +1,9 @@
-def inning(game):
-    pass
+def simulate_inning(game):
+    for half in ['top', 'bottom']:
+        simulate_half_inning(game, half)
+
+
+def simulate_half_inning(game, half):
+    if game.get_inning() == 9 and half == 'bottom' and game.home_score > game.away_score:
+        return
+
