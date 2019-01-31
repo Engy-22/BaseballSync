151302c84e1b1f7cfd2d7870cd8e03589dd4a809
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-31 16:40:13 -0500

    Create batters_pitch_fx_connection.py

---
+++ src/utilities/connections/batters_pitch_fx_connection.py
@@ -0,0 +1,27 @@
+import pymysql
+
+
+class DatabaseConnection:
+    def __init__(self):
+        self.db = pymysql.connect("localhost", "root", "Invader1401asdf", "pitchers_pitch_fx")
+        self.cursor = self.db.cursor()
+
+    def write(self, action):
+        try:
+            self.cursor.execute(action)
+            self.db.commit()
+        except Exception as e:
+            if not any(special_error in str(e) for special_error in ['Duplicate entry','check that column/key exists']):
+                print('\t\t' + str(e) + ' --> ' + action)
+            elif 'deadlock' in str(e):
+                with open('C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\src\\utilities\\deadlocked.txt',
+                          'a') as deadlocked_file:
+                    deadlocked_file.write(action)
+            self.db.rollback()
+
+    def read(self, action):
+        self.cursor.execute(action)
+        return self.cursor.fetchall()
+
+    def close(self):
+        self.db.close()
