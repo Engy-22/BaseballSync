02c7d8aa97d5402c93a328d1e04837f93820b799
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-31 16:38:53 -0500

    Update hof_finder.py

--- src/import_data/player_data/awards/hof_finder.py
+++ src/import_data/player_data/awards/hof_finder.py
@@ -2,7 +2,7 @@ import time
 import datetime
 from utilities.time_converter import time_converter
 from utilities.logger import Logger
-from utilities.baseball_data_connection import DatabaseConnection
+from utilities.connections.baseball_data_connection import DatabaseConnection
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
