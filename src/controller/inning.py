d2fdcb5ab2012b558ab0c16e2586c6d04b37bd96
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-31 14:58:44 -0500

    Update inning.py

--- src/controller/inning.py
+++ src/controller/inning.py
@@ -1,3 +1,6 @@
+from controller.plate_appearance import simulate_plate_appearance
+
+
 def simulate_inning(game):
     for half in ['top', 'bottom']:
         simulate_half_inning(game, half)
@@ -6,3 +9,6 @@ def simulate_inning(game):
 def simulate_half_inning(game, half):
     if game.get_inning() == 9 and half == 'bottom' and game.home_score > game.away_score:
         return
+    outs = 0
+    while outs < 3:
+        simulate_plate_appearance('', '')
