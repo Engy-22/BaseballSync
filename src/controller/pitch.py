6216350c5d669e7c58d4e736f9aef43122e76382
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-31 15:13:30 -0500

    Update pitch.py

--- src/controller/pitch.py
+++ src/controller/pitch.py
@@ -2,4 +2,11 @@ import random
 
 
 def simulate_pitch(balls, strikes):
-    pass
+    pitch_data = {}
+    pitch_data['pitch_type'] = ''
+    pitch_data['ball_strike'] = ''
+    pitch_data['trajectory'] = ''
+    pitch_data['field'] = ''
+    pitch_data['direction'] = ''
+    pitch_data['outcome'] = ''
+    return pitch_data
