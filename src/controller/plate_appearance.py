8f7b68e819790acac715019dd6d0db1b476852f3
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-02-01 17:56:32 -0500

    Update plate_appearance.py

--- src/controller/plate_appearance.py
+++ src/controller/plate_appearance.py
@@ -11,6 +11,7 @@ def simulate_plate_appearance(batter_id, pitcher_id, driver_logger):
     driver_logger.log('\t\tPlate appearance' + batter_id + " vs. " + pitcher_id)
     start_time = time.time()
     logger.log("Simulating plate appearance: " + batter_id + " vs. " + pitcher_id)
+    plate_appearance_data = {}
     plate_appearance = PlateAppearance(batter_id, pitcher_id)
     outcome = None
     while plate_appearance.get_balls() < 4 and plate_appearance.get_strikes() < 3 and outcome is None:
@@ -18,3 +19,4 @@ def simulate_plate_appearance(batter_id, pitcher_id, driver_logger):
     total_time = time_converter(time.time() - start_time)
     logger.log("Done simulating plate appearance: Time = " + total_time + '\n\n')
     driver_logger.log("\t\t\tTime = " + total_time)
+    return plate_appearance_data
