8f508b314e0bee52f9a04203c21cd5e563ec9886
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-02-01 17:50:01 -0500

    Update Inning.py

--- src/model/Inning.py
+++ src/model/Inning.py
@@ -3,6 +3,13 @@ class Inning:
         self.outs = 0
         self.away_runs = 0
         self.home_runs = 0
+        self.half_inning = ''
+
+    def switch_half_inning(self):
+        if self.half_inning == 'top':
+            self.half_inning = 'bottom'
+        else:
+            self.half_inning = 'top'
 
     def increment_outs(self):
         self.outs += 1
@@ -16,4 +23,7 @@ class Inning:
 
     def get_outs(self):
         return self.outs
+
+    def get_half_inning(self):
+        return self.half_inning
 ################### Getters ###################
