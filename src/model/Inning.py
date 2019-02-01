ad20d8552f048dba821589b7c004bed483643b5b
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-02-01 15:32:18 -0500

    Update Inning.py

--- src/model/Inning.py
+++ src/model/Inning.py
@@ -3,3 +3,17 @@ class Inning:
         self.outs = 0
         self.away_runs = 0
         self.home_runs = 0
+
+    def increment_outs(self):
+        self.outs += 1
+
+################### Getters ###################
+    def get_away_runs(self):
+        return self.away_runs
+
+    def get_home_runs(self):
+        return self.home_runs
+
+    def get_outs(self):
+        return self.outs
+################### Getters ###################
