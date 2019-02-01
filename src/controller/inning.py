99a0c2c9725320defa58a643bbd580ae0abf5bcc
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-02-01 17:49:54 -0500

    Update inning.py

--- src/controller/inning.py
+++ src/controller/inning.py
@@ -1,3 +1,4 @@
+from model.Inning import Inning
 from utilities.logger import Logger
 from controller.plate_appearance import simulate_plate_appearance
 import time
@@ -7,23 +8,25 @@ logger = Logger("C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\lo
 
 
 def simulate_inning(game, driver_logger):
-    inning = str(game.get_inning())
-    driver_logger.log('\tInning ' + inning)
+    inning = Inning()
+    inning_num = str(game.get_inning())
+    driver_logger.log('\tInning ' + inning_num)
     start_time = time.time()
-    logger.log("Starting inning simulation: " + game.get_away_team() + " @ " + game.get_home_team() + " - " + inning)
-    for half in ['top', 'bottom']:
-        simulate_half_inning(game, inning, half)
+    logger.log("Starting inning simulation: " + game.get_away_team() + " @ " + game.get_home_team() + " - " + inning_num)
+    for half in range(2):
+        inning.switch_half_inning()
+        simulate_half_inning(game, inning_num, inning.get_half_inning())
     total_time = time_converter(time.time() - start_time)
     driver_logger.log('\t\tTime = ' + total_time)
-    logger.log("Done simulating inning: " + game.get_away_team() + " @ " + game.get_home_team() + " - " + inning
+    logger.log("Done simulating inning: " + game.get_away_team() + " @ " + game.get_home_team() + " - " + inning_num
                + ": Time = " + total_time + '\n\n')
 
 
 def simulate_half_inning(game, inning, half):
     start_time = time.time()
-    logger.log('\tSimulating the ' + half + ' of the ' + inning + ' inning')
     if inning == '9' and half == 'bottom' and game.home_score > game.away_score:
         return
+    logger.log('\tSimulating the ' + half + ' of the ' + inning + ' inning')
     outs = 0
     while outs < 3:
         simulate_plate_appearance('', '', logger)
