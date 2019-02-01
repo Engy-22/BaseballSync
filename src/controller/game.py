3da4b9b75fc6a4957a820ac8562536cbc302a0e6
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-02-01 10:58:26 -0500

    Update game.py

--- src/controller/game.py
+++ src/controller/game.py
@@ -11,6 +11,7 @@ def simulate_game(away_team, home_team, driver_logger):
     driver_logger.log("\tStarting game simulation: " + away_team + " @ " + home_team)
     start_time = time.time()
     logger.log("Starting game simulation: " + away_team + " @ " + home_team)
+    game_data = {}
     game = Game(away_team, home_team)
     while game.get_inning() <= 9 or game.away_score == game.home_score:
         simulate_inning(game, logger)
@@ -18,3 +19,4 @@ def simulate_game(away_team, home_team, driver_logger):
     total_time = time_converter(time.time() - start_time)
     logger.log("Done simulating game: " + away_team + " @ " + home_team + ": Time = " + total_time + '\n\n')
     driver_logger.log("Done simulating game: " + away_team + " @ " + home_team + ": Time = " + total_time)
+    return game_data
