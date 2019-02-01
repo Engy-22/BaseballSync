7a945571c22cc546168bfbab19e43665b97bdd03
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-02-01 11:22:28 -0500

    Update main.py

--- src/controller/main.py
+++ src/controller/main.py
@@ -4,7 +4,7 @@ from utilities.logger import Logger
 from utilities.time_converter import time_converter
 from controller.game import simulate_game
 
-logger = Logger("C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\logs\\controller\\main")
+logger = Logger("C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\logs\\controller\\main.log")
 
 
 start_time = time.time()
