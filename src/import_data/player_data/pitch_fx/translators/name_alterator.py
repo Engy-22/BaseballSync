59e6fe875b8ce28e74ab927b2f21c8a166b7cdd4
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-02-01 11:05:56 -0500

    Update name_alterator.py

--- src/import_data/player_data/pitch_fx/translators/name_alterator.py
+++ src/import_data/player_data/pitch_fx/translators/name_alterator.py
@@ -6,6 +6,6 @@ def name_alterator(first_name, last_name):
               'translators\\pitch_fx_player_name_alterations.csv', 'r') as file:
         rows = csv.reader(file)
         for row in rows:
-            if first_name + '.' + last_name == row[0]:
+            if first_name + ';' + last_name == row[0]:
                 return row[1]
     return None
