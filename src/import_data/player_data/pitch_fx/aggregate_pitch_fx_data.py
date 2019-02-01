3797e460ec3d505043b5bea1b6a26343c371507b
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-01-31 21:09:28 -0500

    Update aggregate_pitch_fx_data.py

--- src/import_data/player_data/pitch_fx/aggregate_pitch_fx_data.py
+++ src/import_data/player_data/pitch_fx/aggregate_pitch_fx_data.py
@@ -1,3 +1,4 @@
+from utilities.connections.baseball_data_connection import DatabaseConnection
 from utilities.connections.pitchers_pitch_fx_connection import PitcherPitchFXDatabaseConnection
 from utilities.connections.batters_pitch_fx_connection import BatterPitchFXDatabaseConnection
 from utilities.logger import Logger
@@ -13,24 +14,21 @@ def aggregate_pitch_fx_data(year, driver_logger):
     start_time = time.time()
     logger.log("Aggregating pitch fx data for " + str(year) + ' || Timestamp: ' + datetime.datetime.today().
                strftime('%Y-%m-%d %H:%M:%S'))
-    aggregate_pitchers(year)
-    aggregate_batters(year)
+    aggregate(year, 'pitching', PitcherPitchFXDatabaseConnection)
+    aggregate(year, 'batting', BatterPitchFXDatabaseConnection)
     total_time = str(time.time() - start_time)
     logger.log("Done aggregating " + str(year) + " pitch fx data: Time = " + total_time)
     driver_logger.log("\t\tTime = " + total_time)
 
 
-def aggregate_pitchers(year):
+def aggregate(year, player_type, db_connection):
     pitcher_time = time.time()
-    logger.log("\tAggregating pitching data")
-    db = PitcherPitchFXDatabaseConnection()
+    logger.log("\tAggregating " + player_type + " data")
+    db = DatabaseConnection()
+    for player_id in set(db.read('select playerid from ' + player_type[:-3] + 'er_pitches')):
+        player_id[0]
     db.close()
-    logger.log("\tDone aggregating pitching data: Time = " + str(time.time() - pitcher_time))
+    logger.log("\tDone aggregating " + player_type + " data: Time = " + str(time.time() - pitcher_time))
 
 
-def aggregate_batters(year):
-    batter_time = time.time()
-    logger.log("\tAggregating batting data")
-    db = BatterPitchFXDatabaseConnection()
-    db.close()
-    logger.log("\tDone aggregating batting data: Time = " + str(time.time() - batter_time))
+# print(len(aggregate(2008, 'pitching', PitcherPitchFXDatabaseConnection)))
