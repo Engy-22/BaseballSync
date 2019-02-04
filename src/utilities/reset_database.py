9ddc1a643a72524b9db1bbde6fcb3393d75eef13
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-02-04 09:38:01 -0500

    Update reset_database.py

--- src/utilities/reset_database.py
+++ src/utilities/reset_database.py
@@ -3,14 +3,21 @@ from utilities.connections.baseball_data_connection import DatabaseConnection
 from concurrent.futures import ThreadPoolExecutor
 
 
-db = DatabaseConnection()
+sandbox_mode = True
+db = DatabaseConnection(sandbox_mode)
 with open("..\\..\\background\\table_definitions.txt", 'rt') as file:
     table_defs = file.readlines()
-    print("removing existing tables")
+    if sandbox_mode:
+        print("removing existing tables - sandbox")
+    else:
+        print("removing existing tables - production")
     with ThreadPoolExecutor(os.cpu_count()) as executor:
         for line in reversed(table_defs):
             executor.submit(db.write("drop table " + line.split('create table ')[1].split(' (')[0]))
-    print("creating new tables")
+    if sandbox_mode:
+        print("creating new tables - sandbox")
+    else:
+        print("creating new tables - production")
     with ThreadPoolExecutor(os.cpu_count()) as executor2:
         for line in table_defs:
             executor2.submit(db.write(line))
