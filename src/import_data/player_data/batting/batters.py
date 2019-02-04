1292e9b0217bb6d5c7e496519af91b1d97fd528b
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-02-04 09:53:19 -0500

    Update batters.py

--- src/import_data/player_data/batting/batters.py
+++ src/import_data/player_data/batting/batters.py
@@ -16,7 +16,7 @@ data = {}
 logger = Logger("C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\\logs\\import_data\\batters.log")
 
 
-def batting_constructor(year, driver_logger):
+def batting_constructor(year, driver_logger, sandbox_mode):
     global data
     data = {}
     print('Downloading batter images and attributes')
@@ -57,7 +57,8 @@ def batting_constructor(year, driver_logger):
         for player_id, dictionary in data.items():
             for team, dictionary2 in dictionary.items():
                 for index, dictionary3 in dictionary2.items():
-                    executor.submit(intermediate, team, index, player_id, dictionary3['temp_player'], dictionary3['row'])
+                    executor.submit(intermediate, team, index, player_id, dictionary3['temp_player'],
+                                    dictionary3['row'], sandbox_mode)
     for player_id, dictionary in data.items():
         for team, dictionary2 in dictionary.items():
             try:
@@ -82,14 +83,14 @@ def extract_player_attributes(player_id, page, reversed_name):
                     'throwsWith': str_ent.split('Throws: </strong>')[1][0]}
 
 
-def intermediate(team, index, player_id, temp_player, row):
-    page = load_url(player_id)
+def intermediate(team, index, player_id, temp_player, row, sandbox_mode):
+    page = load_url(player_id, sandbox_mode)
     if page is not None:
         if "-0" in temp_player:
             reversed_name = temp_player.split("-0")[0].replace("'", "\'")
         else:
             reversed_name = temp_player.split("0")[0].replace("'", "\'")
-        write_to_db(player_id, extract_player_attributes(player_id, page, reversed_name))
+        write_to_db(player_id, extract_player_attributes(player_id, page, reversed_name), sandbox_mode)
     get_stats(player_id, team, row, index)
 
 
@@ -112,9 +113,9 @@ def get_stats(player_id, team, row, index):
     data[player_id][team][index]['stats'] = stat_dictionary
 
 
-def load_url(player_id):
+def load_url(player_id, sandbox_mode):
     page = None
-    db = DatabaseConnection()
+    db = DatabaseConnection(sandbox_mode)
     if len(db.read('select * from players where playerid = "' + player_id + '";')) == 0:
         page = BeautifulSoup(urlopen("https://www.baseball-reference.com/players/" + player_id[0] + "/" + player_id
                                      + ".shtml"), 'html.parser')
@@ -122,19 +123,19 @@ def load_url(player_id):
     return page
 
 
-def write_to_db(player_id, player_attributes):
+def write_to_db(player_id, player_attributes, sandbox_mode):
     fields = ''
     values = ''
     for field, value in player_attributes.items():
         fields += ', ' + field
         values += '", "' + value
-    db = DatabaseConnection()
+    db = DatabaseConnection(sandbox_mode)
     if len(db.read('select * from players where playerid = "' + player_id + '";')) == 0:
         db.write('insert into players (playerid ' + fields + ') values ("' + player_id + values + '");')
     db.close()
 
 
-def write_teams_and_stats(player_id, stats, ratios, team, year):
+def write_teams_and_stats(player_id, stats, ratios, team, year, sandbox_mode):
     stat_nums = {}
     for index, numbers in stats.items():
         for field, value in numbers['stats'].items():
@@ -142,7 +143,7 @@ def write_teams_and_stats(player_id, stats, ratios, team, year):
                 stat_nums[field] += value
             else:
                 stat_nums[field] = value
-    db = DatabaseConnection()
+    db = DatabaseConnection(sandbox_mode)
     if len(db.read('select pt_uniqueidentifier from player_teams where playerid = "' + player_id + '" and teamid = "'
                    + team + '";')) == 0:
         db.write('insert into player_teams (pt_uniqueidentifier, playerid, teamid) values (default, "' + player_id
