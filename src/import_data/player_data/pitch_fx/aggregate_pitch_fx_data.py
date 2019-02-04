1ce85d26496b30530dbd83f2b5986ebe0f80c40a
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-02-04 09:53:32 -0500

    Update aggregate_pitch_fx_data.py

--- src/import_data/player_data/pitch_fx/aggregate_pitch_fx_data.py
+++ src/import_data/player_data/pitch_fx/aggregate_pitch_fx_data.py
@@ -9,22 +9,22 @@ logger = Logger("C:\\Users\\Anthony Raimondo\\PycharmProjects\\baseball-sync\log
                 "aggregate_pitch_fx_data.log")
 
 
-def aggregate_pitch_fx_data(year, driver_logger):
+def aggregate_pitch_fx_data(year, driver_logger, sandbox_mode):
     driver_logger.log("\tAggregating pitch fx data")
     start_time = time.time()
     logger.log("Aggregating pitch fx data for " + str(year) + ' || Timestamp: ' + datetime.datetime.today().
                strftime('%Y-%m-%d %H:%M:%S'))
-    aggregate(year, 'pitching', PitcherPitchFXDatabaseConnection)
-    aggregate(year, 'batting', BatterPitchFXDatabaseConnection)
+    aggregate(year, 'pitching', PitcherPitchFXDatabaseConnection, sandbox_mode)
+    aggregate(year, 'batting', BatterPitchFXDatabaseConnection, sandbox_mode)
     total_time = str(time.time() - start_time)
     logger.log("Done aggregating " + str(year) + " pitch fx data: Time = " + total_time)
     driver_logger.log("\t\tTime = " + total_time)
 
 
-def aggregate(year, player_type, db_connection):
+def aggregate(year, player_type, db_connection, sandbox_mode):
     pitcher_time = time.time()
     logger.log("\tAggregating " + player_type + " data")
-    db = DatabaseConnection()
+    db = DatabaseConnection(sandbox_mode)
     for player_id in set(db.read('select playerid from ' + player_type[:-3] + 'er_pitches')):
         player_id[0]
     db.close()
