4cbe23d2dd93f890120f57e5bc327a63e86b97f4
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-02-05 15:11:14 -0500

    Update translate_pitch_type.py

--- src/import_data/player_data/pitch_fx/translators/translate_pitch_type.py
+++ src/import_data/player_data/pitch_fx/translators/translate_pitch_type.py
@@ -1,5 +1,5 @@
 def translate_pitch_type(pitch_type):
     pitches = {'FF': '4sm', 'FA': 'fb', 'FT': '2sm', 'FC': 'cut', 'FS': 'split', 'FO': 'pitchout', 'SI': 'sink',
-               'SL': 'slide', 'CU': 'curve', 'KC': 'knuckcurve', 'EP': 'eephus', 'CH': 'change', 'SC': 'screw',
+               'SL': 'slide', 'CU': 'curve', 'KC': 'knuckcurve', 'EP': 'eephus', 'CH': 'changeup', 'SC': 'screw',
                'KN': 'knuck'}
     return pitches[pitch_type]
