5cb4aee726b797e193e079b4cda936907d132e6a
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-02-05 14:02:55 -0500

    Update reset_database.py

--- src/utilities/reset_database.py
+++ src/utilities/reset_database.py
@@ -1,5 +1,7 @@
 import os
 from utilities.connections.baseball_data_connection import DatabaseConnection
+from utilities.connections.pitchers_pitch_fx_connection import PitcherPitchFXDatabaseConnection
+from utilities.connections.batters_pitch_fx_connection import BatterPitchFXDatabaseConnection
 from concurrent.futures import ThreadPoolExecutor
 
 
@@ -13,7 +15,7 @@ with open("..\\..\\background\\table_definitions.txt", 'rt') as file:
         print("removing existing tables - production")
     with ThreadPoolExecutor(os.cpu_count()) as executor:
         for line in reversed(table_defs):
-            executor.submit(db.write("drop table " + line.split('create table ')[1].split(' (')[0]))
+            executor.submit(db.write("drop table " + line.split('create table ')[1].split(' (')[0] + ';'))
     if sandbox_mode:
         print("creating new tables - sandbox")
     else:
@@ -21,4 +23,17 @@ with open("..\\..\\background\\table_definitions.txt", 'rt') as file:
     with ThreadPoolExecutor(os.cpu_count()) as executor2:
         for line in table_defs:
             executor2.submit(db.write(line))
+db.close()
+
+db = PitcherPitchFXDatabaseConnection()
+with ThreadPoolExecutor(os.cpu_count()) as executor:
+    tables = db.read('show tables')
+    for delete_table in tables:
+        executor.submit(db.write('drop table ' + delete_table[0] + ';'))
+db.close()
+db = BatterPitchFXDatabaseConnection()
+with ThreadPoolExecutor(os.cpu_count()) as executor:
+    tables = db.read('show tables')
+    for delete_table in tables:
+        executor.submit(db.write('drop table ' + delete_table[0] + ';'))
 db.close()
