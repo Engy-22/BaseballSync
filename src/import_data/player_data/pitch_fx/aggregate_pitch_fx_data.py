00ca0c7513e55aecde42e8105fed41727fc62a52
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-02-06 22:12:46 -0500

    Update aggregate_pitch_fx_data.py

--- src/import_data/player_data/pitch_fx/aggregate_pitch_fx_data.py
+++ src/import_data/player_data/pitch_fx/aggregate_pitch_fx_data.py
@@ -19,7 +19,7 @@ def aggregate_pitch_fx_data(year, driver_logger, sandbox_mode):
     logger.log("Aggregating pitch fx data for " + str(year) + ' || Timestamp: ' + datetime.datetime.today().
                strftime('%Y-%m-%d %H:%M:%S'))
     aggregate_and_write(year, 'pitching', sandbox_mode)
-    aggregate_and_write(year, 'batting', sandbox_mode)
+    # aggregate_and_write(year, 'batting', sandbox_mode)
     total_time = time_converter(time.time() - start_time)
     logger.log("Done aggregating " + str(year) + " pitch fx data: Time = " + total_time)
     driver_logger.log("\t\tTime = " + total_time)
@@ -29,15 +29,16 @@ def aggregate_and_write(year, player_type, sandbox_mode):
     pitcher_time = time.time()
     logger.log("\tAggregating " + player_type + " data and writing to database")
     db = DatabaseConnection(sandbox_mode)
-    players = set(db.read('select playerid from ' + player_type[:-3] + 'er_pitches'))
+    players = set(db.read('select playerid from ' + player_type[:-3] + 'er_pitches;'))
     db.close()
     for player_id in players:
         aggregate(year, player_id[0], player_type, sandbox_mode)
         break
-    logger.log("\tDone aggregating and writing" + player_type + " data: Time = " + str(time.time() - pitcher_time))
+    logger.log("\tDone aggregating and writing " + player_type + " data: Time = " + str(time.time() - pitcher_time))
 
 
 def aggregate(year, player_id, player_type, sandbox_mode):
+    print(player_id)
     table = player_type[:-3] + 'er_pitches'
     matchups = ['vr', 'vl']
     opponent = 'hb' if player_type == 'pitching' else 'hp'
@@ -49,14 +50,20 @@ def aggregate(year, player_id, player_type, sandbox_mode):
     swings_by_pitch_dict = {}
     strikes_by_pitch_dict = {}
     db = DatabaseConnection(sandbox_mode)
-    temp_team_id = db.read('select teamid from player_teams where playerid = "' + player_id + '";')
-    if len(temp_team_id) > 1:
-        team_id = 'TOT'
+    temp_p_uid = []
+    for pt_uid in db.read('select pt_uniqueidentifier from player_teams where playerid = "' + player_id + '";'):
+        try:
+            temp_p_uid.append(db.read('select p' + player_type[0] + '_uniqueidentifier from player_' + player_type
+                                      + ' where year = ' + str(year) + ' and pt_uniqueidentifier = ' + str(pt_uid[0])
+                                      + ';')[0][0])
+        except IndexError:
+            pass
+    if len(temp_p_uid) > 1:
+        p_uid = db.read('select p' + player_type[0] + '_uniqueidentifier from player_' + player_type + ' where year = '
+                        + str(year) + ' and pt_uniqueidentifier = (select pt_uniqueidentifier from player_teams where '
+                        'playerid = "' + player_id + '" and teamid = "TOT")' + ';')[0][0]
     else:
-        team_id = temp_team_id[0][0]
-    p_uid = db.read('select p' + player_type[0] + '_uniqueidentifier from player_' + player_type + ' where year = '
-                    + str(year) + ' and pt_uniqueidentifier = (select pt_uniqueidentifier from player_teams where '
-                    'playerid = "' + player_id + '" and teamid = "' + team_id + '")' + ';')[0][0]
+        p_uid = temp_p_uid[0]
     for matchup in matchups:
         pitches_length_pitch_type[matchup] = {}
         pitches_length_swing_take_and_ball_strike[matchup] = {}
@@ -79,26 +86,26 @@ def aggregate(year, player_id, player_type, sandbox_mode):
                         pitches_length_swing_take_and_ball_strike[matchup][str(ball)+str(strike)][pitch_type[0]] += 1
                     else:
                         pitch_types_dict[matchup][str(ball)+str(strike)][pitch_type[0]] = 1
-                        swings_by_pitch_dict[matchup][str(ball)+str(strike)][pitch_type[0]] = 0
-                        strikes_by_pitch_dict[matchup][str(ball)+str(strike)][pitch_type[0]] = 0
                         pitches_length_swing_take_and_ball_strike[matchup][str(ball)+str(strike)][pitch_type[0]] = 1
                 for pitch_type in set(pitch_types):
-                    bulk_query += ' and pitch_type = "' + pitch_type[0] + '"'
-                    for swing_take in set(db.read('select swing_take ' + bulk_query + ';')):
+                    swings_by_pitch_dict[matchup][str(ball)+str(strike)][pitch_type[0]] = 0
+                    strikes_by_pitch_dict[matchup][str(ball)+str(strike)][pitch_type[0]] = 0
+                    temp_bulk_query = bulk_query + ' and pitch_type = "' + pitch_type[0] + '"'
+                    for swing_take in db.read('select swing_take ' + temp_bulk_query + ';'):
                         if swing_take[0] == 'swing':
                             swings_by_pitch_dict[matchup][str(ball)+str(strike)][pitch_type[0]] += 1
-                        bulk_query += ' and swing_take = "' + swing_take[0] + '"'
-                        for ball_strike in set(db.read('select ball_strike ' + bulk_query + ';')):
+                        temp_bulk_query += ' and swing_take = "' + swing_take[0] + '"'
+                        for ball_strike in db.read('select ball_strike ' + temp_bulk_query + ';'):
                             if ball_strike[0] == 'strike':
                                 strikes_by_pitch_dict[matchup][str(ball)+str(strike)][pitch_type[0]] += 1
-                            bulk_query += ' and ball_strike = "' + ball_strike[0] + '"'
-                            for outcome in set(db.read('select outcome ' + bulk_query + ';')):
-                                bulk_query += ' and outcome = "' + outcome[0] + '"'
-                                for trajectory in set(db.read('select trajectory ' + bulk_query + ';')):
-                                    bulk_query += ' and trajectory = "' + trajectory[0] + '"'
-                                    for field in set(db.read('select field ' + bulk_query + ';')):
-                                        bulk_query += ' and field = "' + field[0] + '"'
-                                        for directions in set(db.read('select direction ' + bulk_query + ';')):
+                            temp_bulk_query += ' and ball_strike = "' + ball_strike[0] + '"'
+                            for outcome in db.read('select outcome ' + temp_bulk_query + ';'):
+                                temp_bulk_query += ' and outcome = "' + outcome[0] + '"'
+                                for trajectory in db.read('select trajectory ' + temp_bulk_query + ';'):
+                                    temp_bulk_query += ' and trajectory = "' + trajectory[0] + '"'
+                                    for field in db.read('select field ' + temp_bulk_query + ';'):
+                                        temp_bulk_query += ' and field = "' + field[0] + '"'
+                                        for directions in db.read('select direction ' + temp_bulk_query + ';'):
                                             count = 0
                                             for direction in directions:
                                                 count += 1
@@ -130,6 +137,7 @@ def write_pitch_usage(player_id, p_uid, year, pitch_type_dict, player_type, leng
 
 
 def write_swing_take_by_pitch(player_id, p_uid, year, swing_take_dict, player_type, length, sandbox_mode):
+    print(swing_take_dict)
     if player_type == 'pitching':
         db = PitcherPitchFXDatabaseConnection(sandbox_mode)
     else:
@@ -151,6 +159,7 @@ def write_swing_take_by_pitch(player_id, p_uid, year, swing_take_dict, player_ty
 
 
 def write_ball_strike_by_pitch(player_id, p_uid, year, ball_strike_dict, player_type, length, sandbox_mode):
+    print(ball_strike_dict)
     if player_type == 'pitching':
         db = PitcherPitchFXDatabaseConnection(sandbox_mode)
     else:
