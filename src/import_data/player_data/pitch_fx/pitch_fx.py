6d2e36af7ebb74a6b6f32d3c62908309671296fe
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-02-13 06:17:46 -0500

    Update pitch_fx.py

--- src/import_data/player_data/pitch_fx/pitch_fx.py
+++ src/import_data/player_data/pitch_fx/pitch_fx.py
@@ -41,10 +41,10 @@ def get_pitch_fx_data(year, driver_logger, sandbox_mode):
     opening_day = db.read('select opening_day from years where year = ' + str(year) + ';')[0][0]
     db.close()
     for month in range(3, 12, 1):
-        if month == 4:
+        if month == 5:
             if month >= int(opening_day.split('-')[0]):
                 for day in range(1, 32, 1):
-                    if day > 12:
+                    if day > 4:
                         if month == int(opening_day.split('-')[0]) and int(day) < int(opening_day.split('-')[1]):
                             continue
                         if len(str(day)) == 1:
