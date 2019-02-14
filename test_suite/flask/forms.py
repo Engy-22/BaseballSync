5e315e4b9d1d0f8117945db5244910310fe2802f
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-02-13 22:18:38 -0500

    Update forms.py

--- test_suite/flask/forms.py
+++ test_suite/flask/forms.py
@@ -12,9 +12,7 @@ class RegistrationForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
-    # username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    # confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
