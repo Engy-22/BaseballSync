339fb9d70ff32fdeab0a953419d0aee7d7316ceb
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-02-16 16:18:12 -0500

    routes

--- src/view/interface/routes.py
+++ src/view/interface/routes.py
@@ -1,26 +1,18 @@
-from flask import Flask, render_template, url_for, flash, redirect
-from flask_sqlalchemy import SQLAlchemy
-from forms import RegistrationForm, LoginForm
 from models import User, Post
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '13e82e0b7361a6e11a886ac181f92c9f'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
 
 
 posts = [
     {
-        'author': 'Corey Schafer',
+        'author': 'Anthony Raimondo',
         'title': 'Blog Post 1',
         'content': 'First post content',
-        'date_posted': 'April 20, 2018'
+        'date_posted': 'February 16, 2019'
     },
     {
-        'author': 'Jane Doe',
+        'author': 'John Doe',
         'title': 'Blog Post 2',
         'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
+        'date_posted': 'February 16, 2019'
     }
 ]
 
@@ -33,14 +25,25 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template('about.html', title='About')
+    return render_template('about.html', title="About")
+
+
+@app.route("/simulate")
+def simulate():
+    return render_template('simulate.html', title="Simulate")
+
+
+@app.route("/fantasy")
+def fantasy():
+    form = LoginForm()
+    return render_template('fantasy.html', title='Fantasy', form=form)
 
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
+        flash(f'Account created for {form.username.data}', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
@@ -57,5 +60,3 @@ def login():
     return render_template('login.html', title='Login', form=form)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
--- test_suite/flask/interface/routes.py
+++ test_suite/flask/interface/routes.py
@@ -1,26 +1,18 @@
-from flask import Flask, render_template, url_for, flash, redirect
-from flask_sqlalchemy import SQLAlchemy
-from forms import RegistrationForm, LoginForm
 from models import User, Post
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '13e82e0b7361a6e11a886ac181f92c9f'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
 
 
 posts = [
     {
-        'author': 'Anthony Raimondo',
+        'author': 'Corey Schafer',
         'title': 'Blog Post 1',
         'content': 'First post content',
-        'date_posted': 'February 16, 2019'
+        'date_posted': 'April 20, 2018'
     },
     {
-        'author': 'John Doe',
+        'author': 'Jane Doe',
         'title': 'Blog Post 2',
         'content': 'Second post content',
-        'date_posted': 'February 16, 2019'
+        'date_posted': 'April 21, 2018'
     }
 ]
 
@@ -33,25 +25,14 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template('about.html', title="About")
-
-
-@app.route("/simulate")
-def simulate():
-    return render_template('simulate.html', title="Simulate")
-
-
-@app.route("/fantasy")
-def fantasy():
-    form = LoginForm()
-    return render_template('fantasy.html', title='Fantasy', form=form)
+    return render_template('about.html', title='About')
 
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}', 'success')
+        flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
@@ -68,5 +49,3 @@ def login():
     return render_template('login.html', title='Login', form=form)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
