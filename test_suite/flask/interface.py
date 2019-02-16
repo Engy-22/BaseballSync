11a15e09e3a4bbff9989c2ceeeac98a8ef475e99
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-02-16 13:51:53 -0500

    Update interface.py

--- test_suite/flask/interface.py
+++ test_suite/flask/interface.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SECRET_KEY'] = '13e82e0b7361a6e11a886ac181f92c9f'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
