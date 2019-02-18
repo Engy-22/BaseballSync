283c95af92df12d093d1eb9187cbb07d70e4c362
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-02-18 12:26:30 -0500

    update user info

--- test_suite/flask/interface/forms.py
+++ test_suite/flask/interface/forms.py
@@ -1,5 +1,7 @@
 from interface.models import User
+from flask_wtf.file import FileField, FileAllowed
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
@@ -25,3 +27,18 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('Update profile picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username and User.query.filter_by(username=username.data).first():
+            raise ValidationError('That username is taken. Please choose a new username.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email and User.query.filter_by(email=email.data).first():
+            raise ValidationError('That email is already in user. Please register with a different email.')
