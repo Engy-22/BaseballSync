0a39492d9e8b082bfe3b6af52ea4ca676c33ba3b
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-03-10 18:09:56 -0400

    minor changes to fantasy.html

--- src/interface/simulate/routes.py
+++ src/interface/simulate/routes.py
@@ -7,10 +7,4 @@ simulate = Blueprint('simulate', __name__)
 @simulate.route("/simulate")
 def simulate_page():
     recent_simulations = ['2/20/2019 CLE vs. DET (50 games)', '2/19/2019 TEX vs. HOU (162 games)']
-    return render_template('simulate/simulate.html', title="Simulate", recent_simulations=recent_simulations,
-                           rand_num='')
-
-
-@simulate.route("/simulate")
-def randomize():
-    flash(random.random(), 'success')
+    return render_template('simulate/simulate.html', title="Simulate", recent_simulations=recent_simulations)
