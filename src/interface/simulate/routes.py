c5abf414365f326c94ae4fbcaec42c220122599b
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-03-10 16:30:24 -0400

    minor fix

--- src/interface/simulate/routes.py
+++ src/interface/simulate/routes.py
@@ -12,7 +12,7 @@ def simulate_page():
 
 
 @simulate.route("/simulate")
-def simulate_page():
+def randomize():
     recent_simulations = ['2/20/2019 CLE vs. DET (50 games)', '2/19/2019 TEX vs. HOU (162 games)']
     return render_template('simulate/simulate.html', title="Simulate", recent_simulations=recent_simulations,
                            rand_num=random.random())
