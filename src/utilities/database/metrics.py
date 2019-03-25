5fc6423615b532bbf0be93bd429236e695914e14
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-03-25 14:47:10 -0400

    made migrate.py os independent

--- src/utilities/database/metrics.py
+++ src/utilities/database/metrics.py
@@ -1,3 +1,4 @@
+import sys
 import tkinter
 from utilities.database.wrappers.baseball_data_connection import DatabaseConnection
 from utilities.database.wrappers.pitchers_pitch_fx_connection import PitcherPitchFXDatabaseConnection
@@ -38,51 +39,94 @@ def gather_batters_pitch_fx_records():
     return tables_records
 
 
-root = tkinter.Tk()
-frame = tkinter.Toplevel(root)
-frame.title("Database Metrics")
-total_tables = 0
-total_records = 0
-baseball_data_tables = 0
-baseball_data_records = 0
-for table, records in gather_baseball_data_records().items():
-    baseball_data_tables += 1
-    total_tables += 1
-    for record in records:
-        baseball_data_records += 1
-        total_records += 1
-pitchers_pitch_fx_tables = 0
-pitchers_pitch_fx_records = 0
-for table, records in gather_pitchers_pitch_fx_records().items():
-    pitchers_pitch_fx_tables += 1
-    total_tables += 1
-    for record in records:
-        pitchers_pitch_fx_records += 1
-        total_records += 1
-batters_pitch_fx_tables = 0
-batters_pitch_fx_records = 0
-for table, records in gather_batters_pitch_fx_records().items():
-    batters_pitch_fx_tables += 1
-    total_tables += 1
-    for record in records:
-        batters_pitch_fx_records += 1
-        total_records += 1
-tkinter.Label(frame, text="Metrics Dashboard", font=font).grid(columnspan=5, padx=10, pady=10)
-tkinter.Label(frame, text="Table Count", font=font).grid(row=2, column=0, padx=5, pady=5, sticky="W")
-tkinter.Label(frame, text="Record Count", font=font).grid(row=3, column=0, padx=5, pady=5, sticky="W")
-tkinter.Label(frame, text="baseballData", font=font).grid(row=1, column=1, padx=10, pady=10)
-tkinter.Label(frame, text=str(num_to_string(baseball_data_tables)), font=font).grid(row=2, column=1, padx=5, pady=5)
-tkinter.Label(frame, text=str(num_to_string(baseball_data_records)), font=font).grid(row=3, column=1, padx=5, pady=5)
-tkinter.Label(frame, text="pitchers_pitch_fx", font=font).grid(row=1, column=2, padx=10, pady=10)
-tkinter.Label(frame, text=str(num_to_string(pitchers_pitch_fx_tables)), font=font).grid(row=2, column=2, padx=5, pady=5)
-tkinter.Label(frame, text=str(num_to_string(pitchers_pitch_fx_records)), font=font).grid(row=3, column=2, padx=5, pady=5)
-tkinter.Label(frame, text="batters_pitch_fx", font=font).grid(row=1, column=3, padx=10, pady=10)
-tkinter.Label(frame, text=str(num_to_string(batters_pitch_fx_tables)), font=font).grid(row=2, column=3, padx=5, pady=5)
-tkinter.Label(frame, text=str(num_to_string(batters_pitch_fx_records)), font=font).grid(row=3, column=3, padx=5, pady=5)
-tkinter.Label(frame, text="Total", font=font).grid(row=1, column=4, padx=10, pady=10)
-tkinter.Label(frame, text=str(num_to_string(total_tables)), font=font).grid(row=2, column=4, padx=5, pady=5)
-tkinter.Label(frame, text=str(num_to_string(total_records)), font=font).grid(row=3, column=4, padx=5, pady=5)
-tkinter.Button(frame, text="Ok", command=lambda: close(), font=font, bg="white", width=6, cursor="hand2").\
-    grid(columnspan=5, padx=10, pady=10)
-root.withdraw()
-root.mainloop()
+def create_table_for_linux():
+    def calculate_spaces(length_of_number, length_of_header):
+        spaces = ''
+        for _ in range((length_of_header-length_of_number)):
+            spaces += ' '
+        return spaces
+    bdh = 'baseballData'
+    ppfxh = 'pitchers_pitch_fx'
+    bpfxh = 'batters_pitch_fx'
+    th = 'Total'
+    tt = str(total_tables)
+    tr = num_to_string(total_records)
+    bdt = str(baseball_data_tables)
+    bdr = num_to_string(baseball_data_records)
+    ppfxt = str(pitchers_pitch_fx_tables)
+    ppfxr = num_to_string(pitchers_pitch_fx_records)
+    bpfxt = str(batters_pitch_fx_tables)
+    bpfxr = num_to_string(batters_pitch_fx_records)
+    beginning_spaces = calculate_spaces(0, len('Records  '))
+    length = ''
+    for _ in range(len(bdh)+len(ppfxh)+len(bpfxh)+32):
+        length += '-'
+    print(beginning_spaces + length + '\n' + beginning_spaces + '|  ' + bdh + '  |  ' + ppfxh + '  |  ' + bpfxh
+          + '  |     ' + th + '     |\n' + beginning_spaces + length + '\nTables   |  ' + bdt
+          + calculate_spaces(len(bdt), len(bdh)) + '  |  ' + ppfxt + calculate_spaces(len(ppfxt), len(ppfxh)) + '  |  '
+          + bpfxt + calculate_spaces(len(bpfxt), len(bpfxh)) + '  |  ' + tt + calculate_spaces(len(tt), 11) + '  |\n'
+          + beginning_spaces + length + '\nRecords  |  ' + bdr + calculate_spaces(len(bdr), len(bdh)) + '  |  ' + ppfxr
+          + calculate_spaces(len(ppfxr), len(ppfxh)) + '  |  ' + bpfxr + calculate_spaces(len(bpfxr), len(bpfxh))
+          + '  |  ' + tr + calculate_spaces(len(tr), 11) + '  |  \n' + beginning_spaces + length)
+
+
+if __name__ == '__main__':
+    total_tables = 0
+    total_records = 0
+    baseball_data_tables = 0
+    baseball_data_records = 0
+    for table, records in gather_baseball_data_records().items():
+        baseball_data_tables += 1
+        total_tables += 1
+        for record in records:
+            baseball_data_records += 1
+            total_records += 1
+    pitchers_pitch_fx_tables = 0
+    pitchers_pitch_fx_records = 0
+    for table, records in gather_pitchers_pitch_fx_records().items():
+        pitchers_pitch_fx_tables += 1
+        total_tables += 1
+        for record in records:
+            pitchers_pitch_fx_records += 1
+            total_records += 1
+    batters_pitch_fx_tables = 0
+    batters_pitch_fx_records = 0
+    for table, records in gather_batters_pitch_fx_records().items():
+        batters_pitch_fx_tables += 1
+        total_tables += 1
+        for record in records:
+            batters_pitch_fx_records += 1
+            total_records += 1
+    if 'win' in sys.platform:
+        root = tkinter.Tk()
+        frame = tkinter.Toplevel(root)
+        frame.title("Database Metrics")
+        tkinter.Label(frame, text="Metrics Dashboard", font=font).grid(columnspan=5, padx=10, pady=10)
+        tkinter.Label(frame, text="Table Count", font=font).grid(row=2, column=0, padx=5, pady=5, sticky="W")
+        tkinter.Label(frame, text="Record Count", font=font).grid(row=3, column=0, padx=5, pady=5, sticky="W")
+        tkinter.Label(frame, text="baseballData", font=font).grid(row=1, column=1, padx=10, pady=10)
+        tkinter.Label(frame, text=str(num_to_string(baseball_data_tables)), font=font).grid(row=2, column=1, padx=5,
+                                                                                            pady=5)
+        tkinter.Label(frame, text=str(num_to_string(baseball_data_records)), font=font).grid(row=3, column=1, padx=5,
+                                                                                             pady=5)
+        tkinter.Label(frame, text="pitchers_pitch_fx", font=font).grid(row=1, column=2, padx=10, pady=10)
+        tkinter.Label(frame, text=str(num_to_string(pitchers_pitch_fx_tables)), font=font).grid(row=2, column=2, padx=5,
+                                                                                                pady=5)
+        tkinter.Label(frame, text=str(num_to_string(pitchers_pitch_fx_records)), font=font).grid(row=3, column=2,
+                                                                                                 padx=5, pady=5)
+        tkinter.Label(frame, text="batters_pitch_fx", font=font).grid(row=1, column=3, padx=10, pady=10)
+        tkinter.Label(frame, text=str(num_to_string(batters_pitch_fx_tables)), font=font).grid(row=2, column=3, padx=5,
+                                                                                               pady=5)
+        tkinter.Label(frame, text=str(num_to_string(batters_pitch_fx_records)), font=font).grid(row=3, column=3, padx=5,
+                                                                                                pady=5)
+        tkinter.Label(frame, text="Total", font=font).grid(row=1, column=4, padx=10, pady=10)
+        tkinter.Label(frame, text=str(num_to_string(total_tables)), font=font).grid(row=2, column=4, padx=5, pady=5)
+        tkinter.Label(frame, text=str(num_to_string(total_records)), font=font).grid(row=3, column=4, padx=5, pady=5)
+        tkinter.Button(frame, text="Ok", command=lambda: close(), font=font, bg="white", width=6, cursor="hand2").\
+            grid(columnspan=5, padx=10, pady=10)
+        root.withdraw()
+        root.mainloop()
+    elif 'linux' in sys.platform:
+        create_table_for_linux()
+    else:
+        print('Unknown operating system. Must use Windows or Linux')
