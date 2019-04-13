84700673841e03bb5197aff07a2ec70211d52423
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-04-13 09:56:59 -0400

    added swap teams and remove team functionality

--- src/interface/static/js/quick_sim.js
+++ src/interface/static/js/quick_sim.js
@@ -1,9 +1,37 @@
 $(document).ready(function() {
+
     $('#matchup').hide();
+
+    $('.remove_team').click(function() {
+        remove_matchup();
+        var this_team = $(this).parent().parent().parent().find('img').attr('id').split('_', 1)[0];
+        $('#' + this_team + '_team').attr('id', '')
+                                    .attr('class', 'unselected_team');
+    }); //end remove_team click function
+
+    $('.swap-button').click(function() {
+        $('#away_team').attr('id', 'temp_team');
+        $('#home_team').attr('id', 'away_team');
+        $('#temp_team').attr('id', 'home_team');
+        $('#away_matchup').fadeOut();
+        $('#home_matchup').fadeOut(function() {
+            var temp_src = $('#away_matchup').attr('src');
+            $('#away_matchup').attr('src', $('#home_matchup').attr('src'));
+            $('#home_matchup').attr('src', temp_src);
+            $('#away_matchup').fadeIn();
+            $('#home_matchup').fadeIn();
+            $('.swap-button').blur();
+        }); //end anonymous function
+    }); //end swap-button click function
+
     $('.team-entity').on('click', function() {
         $(this).find('img').toggleClass('unselected_team selected_team'); //change selected team to be highlighted or unhighlighted
         if ($('.selected_team').length == 2) {
-            $(this).find('img').attr('id', 'home_team');
+            if ($('#home_team').length > 0) {
+                $(this).find('img').attr('id', 'away_team');
+            } else {
+                $(this).find('img').attr('id', 'home_team');
+            } //end if-else
             $('.unselected_team').parent('figure').addClass('hide-figure');
             hide_table();
         } else {
@@ -19,7 +47,7 @@ $(document).ready(function() {
 
 
 function hide_table() {
-    $('.simulate-content-section').fadeOut(function() { //hide all teams that were not selected
+    $('.simulate-content-section').fadeOut(750, function() { //hide all teams that were not selected
         display_matchup();
         return false;
     });
@@ -27,20 +55,20 @@ function hide_table() {
 
 
 function display_matchup() {
-    document.getElementById('away_matchup').src = $('#away_team').attr('src');
-    document.getElementById('home_matchup').src = $('#home_team').attr('src');
-    $('#matchup').removeClass('hidden');
-    $('#matchup').fadeIn(750);
-}; //end move_teams function
+    $('#away_matchup').attr('src', $('#away_team').attr('src'));
+    $('#home_matchup').attr('src', $('#home_team').attr('src'));
+    $('#matchup').removeClass('hidden')
+                 .fadeIn(750); //show the matchup of the two selected teams
+};
 
 
 function show_table() {
-    $('.simulate-content-section').fadeIn(function() { //show all teams that were previously hidden
-        remove_matchup();
-    });
+    $('.simulate-content-section').fadeIn(750); //show the entire league table again
 };
 
 
 function remove_matchup() {
-    $('#matchup').fadeOut(750);
-}; //end move_teams function
+    $('#matchup').fadeOut(750, function() { //hide the matchup
+        show_table();
+    });
+};
