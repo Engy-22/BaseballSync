6eb0610f5740d4984fda2df084ed23bf9529ef65
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-04-15 18:41:34 -0400

    fix to simulsync

--- src/simulsync/fetch.py
+++ src/simulsync/fetch.py
@@ -2,7 +2,7 @@ import os
 import time
 import datetime
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
+from urllib.request import urlopen, urlretrieve
 from xml.dom import minidom
 from concurrent.futures import ThreadPoolExecutor
 from utilities.properties import simulsync_driver_logger
@@ -19,13 +19,54 @@ def fetch_home_pages():
         day = '0' + day
     if len(str(month)) == 1:
         month = '0' + month
-    home_page_url = 'http://gd2.mlb.com/components/game/mlb/year_' + str(date.year) + '/month_' + month + '/day_' + '28'
+    home_page_url = 'http://gd2.mlb.com/components/game/mlb/year_' + str(date.year) + '/month_' + month + '/day_' + day
     # home_page_url = 'http://gd2.mlb.com/components/game/mlb/year_2018/month_05/day_07'
     home_page = str(BeautifulSoup(urlopen(home_page_url), 'html.parser')).split('<li>')
     simulsync_driver_logger.log('\tTime = ' + time_converter(time.time()-start_time))
     return [home_page_url + url for url in home_page]
 
 
-def fetch_game(url):
-    game_page = str(BeautifulSoup(urlopen(url), 'html.parser'))
-    return game_page
+def fetch_game(url, past_game_data):
+    scoreboard = str(BeautifulSoup(urlopen(url + 'miniscoreboard.xml'), 'html.parser'))
+    game_status = scoreboard.split('<game_status ')[1].split('status=')[1].split('"')[1]
+    if game_status in ['Final', 'Game Over']:
+        return 'completed', None
+    elif game_status == "In Progress":
+        game_page = str(BeautifulSoup(urlopen(url + 'inning/'), 'html.parser')).split('<li>')
+        innings = []
+        for inning in game_page:
+            try:
+                if inning.split('<a href="inning_')[1].split('.')[0].isdigit():
+                    innings.append(inning.split('<a href=')[1].split('"')[1])
+            except IndexError:
+                continue
+        try:
+            urlretrieve(url + 'inning/' + innings[-1], os.path.join('simulsync', 'simulsync.xml'))
+        except Exception:
+            return 'in progress', 'get_the_latest_pitch'
+    else:
+        return game_status, None
+    return 'in progress', get_latest_pitch(past_game_data)
+
+
+def get_latest_pitch(past_game_data):
+    try:
+        simulsync_xml = minidom.parse(os.path.join('simulsync', 'simulsync.xml'))
+        if past_game_data['latest_pitch'] != 'get_the_latest_pitch':
+            latest_pitch_found = False
+            latest_pitch = past_game_data['latest_pitch']
+            for at_bat in simulsync_xml.getElementsByTagName('atbat'):
+                for pitch in at_bat.getElementsByTagName('pitch'):
+                    if latest_pitch_found:
+                        latest_pitch = pitch.getAttribute('play_guid')
+                        print('\t' + pitch.getAttribute('pitch_type'))
+                    else:
+                        if pitch.getAttribute('play_guid') == past_game_data['latest_pitch']:
+                            latest_pitch_found = True
+            return latest_pitch
+        else:
+            latest_at_bat = simulsync_xml.getElementsByTagName('atbat')[-1]
+            print('\t' + latest_at_bat.getElementsByTagName('pitch')[-1].getAttribute('pitch_type'))
+            return latest_at_bat.getElementsByTagName('pitch')[-1].getAttribute('play_guid')
+    except IndexError:
+        return past_game_data['latest_pitch']
--- src/simulsync_service.py
+++ src/simulsync_service.py
@@ -1,17 +1,41 @@
 import os
 import sys
+import time
 from simulsync.fetch import fetch_home_pages, fetch_game
 
 sys.path.append(os.path.join(sys.path[0], '..', '..'))
 
 
 def driver():
-    for url in fetch_home_pages():
-        if 'gid' in url:
+    completed_games = []
+    games_in_progress = {}
+    while True:
+        for url in fetch_home_pages():
             try:
-                print('\n' + fetch_game(url.split('<a href="')[0]+url.split('<a href="')[1].split('">')[0]))
+                this_game = url.split('<a href=')[1].split('_')[5:7]
+                if len(this_game) > 0:
+                    print(this_game)
             except IndexError:
-                continue
+                pass
+            if url not in completed_games:
+                if url not in games_in_progress:
+                    games_in_progress[url] = {'latest_pitch': 'get_the_latest_pitch'}
+                if 'gid' in url:
+                    try:
+                        game, latest_pitch = \
+                            fetch_game(url.split('<a href="')[0] + '/' + url.split('<a href="')[1].split('/')[1] + '/',
+                                       games_in_progress[url])
+                        if game == 'completed':
+                            completed_games.append(url)
+                            del games_in_progress[url]
+                        elif game == 'in progress':
+                            games_in_progress[url]['latest_pitch'] = latest_pitch
+                        else:
+                            continue
+                    except IndexError:
+                        continue
+        print('\n')
+        time.sleep(10)
 
 
 driver()
