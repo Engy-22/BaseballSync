d965da3a7ea39d5b4b76af42d5949c3ac1d52418
Anthony Raimondo <anthonyraimondo7@gmail.com>
2019-06-12 13:06:26 -0400

    started consolidating player stats

---
+++ src/import_data/consolidata/player_stats.py
@@ -0,0 +1,9 @@
+from utilities.database.wrappers.baseball_data_connection import DatabaseConnection
+
+
+def consolidate_pitcher_stats(pp_uid):
+    pass
+
+
+def consolidate_hitter_stats(pb_uid):
+    pass
--- src/model/players/player.py
+++ src/model/players/player.py
@@ -59,20 +59,20 @@ class Player:
     def retrieve_batting_stats(self):
         with open(os.path.join("..", "background", "batting_pitch_fx_tables.csv")) as tables_file:
             tables = tables_file.readlines()
-        db = DatabaseConnection(sandbox_mode=True)
+        db = DatabaseConnection(sandbox_mode=False)
         for table in tables:
-            print(db.read('select * from ' + table + ' where playerid = "' + self.player_id + ' and year = '
-                          + str(self.year) + '";'))
+            print(table, db.read('select * from ' + table + ' where playerid = "' + self.player_id + '" and year = '
+                                 + str(self.year) + ';'))
         db.close()
         self.batting_stats = 'batting stats'
 
     def retrieve_pitching_stats(self):
         with open(os.path.join("..", "background", "pitching_pitch_fx_tables.csv")) as tables_file:
             tables = tables_file.readlines()
-        db = DatabaseConnection(sandbox_mode=True)
+        db = DatabaseConnection(sandbox_mode=False)
         for table in tables:
-            print(db.read('select * from ' + table[:-1] + ' where playerid = "' + self.player_id + '" and year = '
-                          + str(self.year) + ';'))
+            print(table, db.read('select * from ' + table[:-1] + ' where playerid = "' + self.player_id + '" and year = '
+                                 + str(self.year) + ';'))
         db.close()
         self.pitching_stats = 'pitching stats'
 
